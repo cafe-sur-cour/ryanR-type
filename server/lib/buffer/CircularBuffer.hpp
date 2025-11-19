de2f8f5467f545cf0a885d9e0fa6b926875411fa
Albane Merian <albane.merian@epitech.eu>
2025-11-19 15:37:55 +0100

    feat(server): Add the implementation of a circular buffer following the buffer interface

---
+++ server/lib/buffer/CircularBuffer.hpp
@@ -0,0 +1,60 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** Buffer
+*/
+
+#include "IBuffer.hpp"
+
+#ifndef CircularBuffer_HPP_
+#define CircularBuffer_HPP_
+
+enum class OverflowPolicy {
+    OVERWRITE,
+    REJECT,
+    EXPAND
+};
+
+class CircularBuffer : public IBuffer {
+    public:
+        CircularBuffer();
+        ~CircularBuffer() override;
+
+        void createBuffer(size_t size) override;
+        void deleteBuffer() override;
+        void clear() override;
+        bool writeBuffer(const char* data, size_t size) override;
+        size_t readBuffer(char* data, size_t size) override;
+        size_t getCapacity() const override;
+        size_t getUsedSize() const override;
+        size_t getAvailableSize() const override;
+        bool isEmpty() const override;
+        bool isFull() const override;
+        char* getBuffer() const override;
+
+
+        /* This function allows use to read a section of the buffer without deleting it contrary ti the read buffer */
+        size_t peek(char* data, size_t size, size_t offset = 0) const;
+        void setOverflowPolicy(OverflowPolicy policy);
+        OverflowPolicy getOverflowPolicy() const;
+
+    private:
+        char *_buffer;
+        size_t _capacity;
+        size_t _readPos;
+        size_t _writePos;
+        size_t _usedSize;
+        OverflowPolicy _overflowPolicy;
+
+        size_t _getNextPos(size_t pos, size_t offset) const;
+        void _advanceReadPos(size_t count);
+        void _advanceWritePos(size_t count);
+};
+
+extern "C" {
+    IBuffer *createBufferInstance();
+    int getType();
+}
+
+#endif /* !BUFFER_HPP_ */
