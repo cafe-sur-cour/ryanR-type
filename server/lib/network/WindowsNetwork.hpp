96d94044852c33d22ab143d0290601a1acaae6b2
Albane Merian <albane.merian@epitech.eu>
2025-11-19 15:44:31 +0100

    feat(server): Add the initialization of the network module for Windows architecture

---
+++ server/lib/network/WindowsNetwork.hpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** WindowsNetwork
+*/
+#include "ANetwork.hpp"
+
+#ifndef WINDOWSNETWORK_HPP_
+#define WINDOWSNETWORK_HPP_
+
+class WindowsNetwork : public ANetwork {
+    public:
+        WindowsNetwork();
+        ~WindowsNetwork() override;
+
+        void init() override;
+        void stop() override;
+        void sendData(const IPacket &data, size_t size) override;
+        IPacket &receiveData(char *buffer, size_t size) const override;
+    protected:
+    private:
+};
+
+extern "C" {
+    INetwork *createNetworkInstance();
+    int getType();
+}
+
+#endif /* !WINDOWSNETWORK_HPP_ */
