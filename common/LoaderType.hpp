795cea30cd6dac5fb18ba867b1df60b82a19d10a
Albane Merian <albane.merian@epitech.eu>
2025-11-19 17:16:31 +0100

    fix(loader): Update the loader type so that the enum type values have value atributed

--- common/LoaderType.hpp
+++ common/LoaderType.hpp
@@ -9,11 +9,11 @@
 #define LOADERTYPE_HPP_
 
 enum ModuleType_t{
-    DISPLAY_MODULE, /* 0 */
-    NETWORK_MODULE, /* 1 */
-    PACKET_MODULE, /* 2 */
-    BUFFER_MODULE, /* 3 */
-    UNKNOWN_MODULE /* 4 */
+    DISPLAY_MODULE = 0,
+    NETWORK_MODULE = 1,
+    PACKET_MODULE = 2,
+    BUFFER_MODULE = 3,
+    UNKNOWN_MODULE
 };
 
 typedef ModuleType_t (*getTypeFunc_t)();
