e300706ed5deff59cee2a0daf88f971de06a0d46
Marin <marin.lamy@epitech.eu>
2025-11-19 14:49:10 +0100

    feat(component): create IComponent interface with constructor and destructor

---
+++ common/ECS/component/IComponent.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** IComponent
+*/
+
+#ifndef ICOMPONENT_HPP_
+#define ICOMPONENT_HPP_
+
+class IComponent {
+    public:
+        IComponent();
+        ~IComponent();
+
+    protected:
+    private:
+};
+
+#endif /* !ICOMPONENT_HPP_ */
