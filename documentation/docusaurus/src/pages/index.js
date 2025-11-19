630808f96f05f55b9dbf769c5eb6f5cfc5a32943
Albane Merian <albane.merian@epitech.eu>
2025-11-19 13:03:44 +0100

    docs(Docusarus): Add the basis of the architecture of the docusaurus web documentation

---
+++ documentation/docusaurus/src/pages/index.js
@@ -0,0 +1,59 @@
+import clsx from 'clsx';
+import Link from '@docusaurus/Link';
+import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
+import Layout from '@theme/Layout';
+import HomepageFeatures from '@site/src/components/HomepageFeatures';
+
+import Heading from '@theme/Heading';
+import styles from './index.module.css';
+
+function HomepageHeader() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <header className={clsx('hero hero--primary', styles.heroBanner)}>
+      <div className="container">
+        <Heading as="h1" className="hero__title">
+          {siteConfig.title}
+        </Heading>
+        <p className="hero__subtitle">{siteConfig.tagline}</p>
+        <div className={styles.buttons}>
+          <Link className="button button--secondary button--lg" to="docs/category/introduction" style={{ margin: '1rem', fontSize: '1.2rem' }}>
+            🌇 Project overview
+          </Link>
+          <Link className="button button--secondary button--lg" to="docs/category/how-to-contributes" style={{ margin: '1rem', fontSize: '1.2rem' }}>
+            🧱 How to contribute
+          </Link>
+          <Link className="button button--secondary button--lg" to="docs/category/client-documentation" style={{ margin: '1rem', fontSize: '1.2rem' }}>
+            🏗️ Client Architecture
+          </Link>
+          <Link className="button button--secondary button--lg" to="docs/category/server-documentation" style={{ margin: '1rem', fontSize: '1.2rem' }}>
+            📲 Server Architecture
+          </Link>
+          <Link className="button button--secondary button--lg" to="docs/category/ecs-documentation" style={{ margin: '1rem', fontSize: '1.2rem' }}>
+            📲 ECS Communication
+          </Link>
+          <Link className="button button--secondary button--lg" to="docs/category/pocs-documentation" style={{ margin: '1rem', fontSize: '1.2rem' }}>
+            📲 POCS and librairy choices
+          </Link>
+          <Link className="button button--secondary button--lg" to="docs/category/security-documentation" style={{ margin: '1rem', fontSize: '1.2rem' }}>
+            🪪 Security
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+}
+
+export default function Home() {
+  const {siteConfig} = useDocusaurusContext();
+  return (
+    <Layout
+      title={`Hello from ${siteConfig.title}`}
+      description="Description will go into a meta tag in <head />">
+      <HomepageHeader />
+      <main>
+        <HomepageFeatures />
+      </main>
+    </Layout>
+  );
+}
