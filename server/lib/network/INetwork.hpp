c3a7aafebcf796866c6d7dc6b09d30ec3990605a
Albane Merian <albane.merian@epitech.eu>
2025-11-19 17:50:15 +0100

    fix(server): Remove the reference to a char *buffer in the Interface network

--- server/lib/network/INetwork.hpp
+++ server/lib/network/INetwork.hpp
@@ -8,6 +8,7 @@
 #include <stdio.h>
 
 #include "../packet/IPacket.hpp"
+#include "../buffer/IBuffer.hpp"
 
 #ifndef INETWORK_HPP_
 #define INETWORK_HPP_
@@ -20,7 +21,7 @@ class INetwork {
         virtual void init() = 0;
         virtual void stop() = 0;
         virtual void sendData(const IPacket &data, size_t size) = 0;
-        virtual IPacket &receiveData(char *buffer, size_t size) const = 0;
+        virtual IPacket &receiveData(const IBuffer &buffer, size_t size) const = 0;
 
     protected:
     private:
