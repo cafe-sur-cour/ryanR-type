eed61ca73e78d2da33e01953f6565593c2b59819
Albane Merian <albane.merian@epitech.eu>
2025-11-19 15:32:35 +0100

    feat(Server): Add an empty header in the Server file so it doesn't cause a coding style error

---
+++ server/ServerConfig.hpp
@@ -0,0 +1,20 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** ServerConfig
+*/
+
+#ifndef SERVERCONFIG_HPP_
+#define SERVERCONFIG_HPP_
+
+class ServerConfig {
+    public:
+        ServerConfig();
+        ~ServerConfig();
+
+    protected:
+    private:
+};
+
+#endif /* !SERVERCONFIG_HPP_ */
