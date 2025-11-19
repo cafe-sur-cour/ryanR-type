b8a68447a62bfee83255fee7079869f20ccb96ff
Albane Merian <albane.merian@epitech.eu>
2025-11-19 15:45:56 +0100

    feat(server): Add the definition of the big endian serialization class

---
+++ server/lib/packet/BigEndianSerialization.cpp
@@ -0,0 +1,24 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** BigEndianSerialization
+*/
+
+#include <vector>
+
+#include "BigEndianSerialization.hpp"
+
+BigEndianSerialization::BigEndianSerialization() {
+}
+
+BigEndianSerialization::~BigEndianSerialization() {
+}
+
+std::vector<uint8_t> BigEndianSerialization::serialize(const void* data,
+    size_t size) {
+}
+
+bool BigEndianSerialization::deserialize(const std::vector<uint8_t>& data,
+    void* output, size_t expectedSize) {
+}
