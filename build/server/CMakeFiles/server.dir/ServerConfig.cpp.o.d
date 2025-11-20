037973bf3e37cc6c51f19bbe574fedbfcdf92e2c
Matisse-M <matisse.marsac@epitech.eu>
2025-11-20 10:56:23 +0100

    rm(r-type): remove useless .DS_Store file and build/ folder

--- build/server/CMakeFiles/server.dir/ServerConfig.cpp.o.d
+++
@@ -1,3 +0,0 @@
-server/CMakeFiles/server.dir/ServerConfig.cpp.o: \
-  /Users/matissemarsac/EPITECH/tek3/R-Type/server/ServerConfig.cpp \
-  /Users/matissemarsac/EPITECH/tek3/R-Type/server/ServerConfig.hpp
