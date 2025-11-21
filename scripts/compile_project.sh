4173f8299bfdff1b9f93eab03cf9ff661ed33505
albanrss <alban.roussee@epitech.eu>
2025-11-21 21:27:20 +0100

    feat(scripts): addede a script to build the project

---
+++ scripts/compile_project.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ -z "$VCPKG_ROOT" ]; then
+    echo "Error: VCPKG_ROOT environment variable is not set."
+    exit 1
+fi
+
+if [ "$1" == "--help" ] || [ "$1" == "-h" ]; then
+    echo "Usage: $0 [<target>]"
+    echo "  <target> : (Optional) The specific target to build."
+    exit 0
+fi
+
+target=$1
+
+cmake --preset "release"
+
+if [ -z "$target" ]; then
+    cmake --build --preset "release"
+    exit $?
+fi
+
+cmake --build --preset "release" --target "$target"
