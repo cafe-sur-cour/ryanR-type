48a4063687d7bc201962ef5c12876d12d5b03329
eliotttesnier <eliott.tesnier@epitech.eu>
2025-11-21 10:13:30 +0100

    feat(states): add AGameState and IGameState classes in the server

---
+++ server/gsm/states/AGameState.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "IGameState.hpp"
+
+namespace gsm {
+
+class AGameState : public IGameState {
+public:
+    AGameState(std::shared_ptr<IGameStateMachine> gsm);
+    ~AGameState() override = default;
+
+    void enter() override;
+    void update(float deltaTime) override;
+    void exit() override;
+
+protected:
+    std::shared_ptr<IGameStateMachine> _gsm;
+};
+
+}
