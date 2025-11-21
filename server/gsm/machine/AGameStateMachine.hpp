1b9430d1db6af5bbe873f4ee3633f71395e17d54
eliotttesnier <eliott.tesnier@epitech.eu>
2025-11-21 10:13:44 +0100

    feat(states): implement AGameStateMachine and IGameStateMachine interfaces in the server

---
+++ server/gsm/machine/AGameStateMachine.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "IGameStateMachine.hpp"
+#include "../states/IGameState.hpp"
+
+namespace gsm {
+
+class AGameStateMachine : public IGameStateMachine {
+public:
+    AGameStateMachine();
+    ~AGameStateMachine() override = default;
+
+    void changeState(std::shared_ptr<IGameState> newState) override;
+    void pushState(std::shared_ptr<IGameState> newState) override;
+    void popState() override;
+
+    void update(float deltaTime) override;
+
+protected:
+    std::stack<std::shared_ptr<IGameState>> states_;
+};
+
+}
