2eab68546edb04d3b9ec2e8c514bfef6e4dd027c
eliotttesnier <eliott.tesnier@epitech.eu>
2025-11-21 10:14:04 +0100

    feat(states): implement AGameStateMachine and IGameStateMachine interfaces in the client

---
+++ client/gsm/machine/AGameStateMachine.cpp
@@ -0,0 +1,47 @@
+#include "AGameStateMachine.hpp"
+#include "../states/IGameState.hpp"
+
+namespace gsm {
+
+AGameStateMachine::AGameStateMachine() {}
+
+void AGameStateMachine::changeState(std::shared_ptr<IGameState> newState) {
+    if (!states_.empty()) {
+        states_.top()->exit();
+        states_.pop();
+    }
+    states_.push(newState);
+    states_.top()->enter();
+}
+
+void AGameStateMachine::pushState(std::shared_ptr<IGameState> newState) {
+    if (!states_.empty()) {
+        states_.top()->exit();
+    }
+    states_.push(newState);
+    states_.top()->enter();
+}
+
+void AGameStateMachine::popState() {
+    if (!states_.empty()) {
+        states_.top()->exit();
+        states_.pop();
+    }
+    if (!states_.empty()) {
+        states_.top()->enter();
+    }
+}
+
+void AGameStateMachine::update(float deltaTime) {
+    if (!states_.empty()) {
+        states_.top()->update(deltaTime);
+    }
+}
+
+void AGameStateMachine::render() {
+    if (!states_.empty()) {
+        states_.top()->render();
+    }
+}
+
+}
