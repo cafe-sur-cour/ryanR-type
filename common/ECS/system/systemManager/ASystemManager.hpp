fef9f182d9f79a7753102a1291dbc51b94bb7a0e
Marin <marin.lamy@epitech.eu>
2025-11-24 14:26:44 +0100

    refactor(system): replace AContext with ResourceManager and ARegistry in system interfaces

--- common/ECS/system/systemManager/ASystemManager.hpp
+++ common/ECS/system/systemManager/ASystemManager.hpp
@@ -12,7 +12,8 @@
 #include <memory>
 
 #include "ISystemManager.hpp"
-#include "../../context/AContext.hpp"
+#include "../../resourceManager/ResourceManager.hpp"
+#include "../../entity/registry/ARegistry.hpp"
 #include "../ISystem.hpp"
 
 namespace ecs {
@@ -21,7 +22,7 @@ class ASystemManager : public ISystemManager {
     public:
         ASystemManager();
         ~ASystemManager();
-        void updateAllSystems(std::shared_ptr<AContext> context, float deltaTime) override;
+        void updateAllSystems(std::shared_ptr<ResourceManager> resourceManager, std::shared_ptr<ARegistry> registry, float deltaTime) override;
         void addSystem(std::shared_ptr<ISystem> system) override;
         void removeSystem(std::shared_ptr<ISystem> system) override;
 
