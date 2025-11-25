656be27556bab68b8c28b824172f376dabf3602a
Matisse-M <matisse.marsac@epitech.eu>
2025-11-25 15:28:30 +0100

    test(network): add some tests about body serialization and update tests about header serialization

--- tests/server/packet/packettest.cpp
+++ tests/server/packet/packettest.cpp
@@ -35,31 +35,54 @@ TEST_F(PacketTest, SettersAndGettersWork) {
 }
 
 TEST_F(PacketTest, PackHeaderPacketProducesCorrectHeader) {
-    Packet packet(1001);
+    Packet packet(1);
     packet.setType(0x02);
-    packet.setLength(256);
 
-    unsigned int clientId = 5555;
-    std::vector<uint8_t> header = packet.packHeaderPacket(clientId);
+    std::vector<uint8_t> header = packet.packHeaderPacket(10, 1, 0x02);
 
-    EXPECT_EQ(header.size(), 24);
+    EXPECT_EQ(header.size(), 16);
     EXPECT_EQ(header[0], 0x93);
+    EXPECT_EQ(header[1], 0x00);
+    EXPECT_EQ(header[2], 0x00);
+    EXPECT_EQ(header[3], 0x00);
+    EXPECT_EQ(header[4], 0x0a);
+    EXPECT_EQ(header[5], 0x00);
+    EXPECT_EQ(header[6], 0x00);
+    EXPECT_EQ(header[7], 0x00);
+    EXPECT_EQ(header[8], 0x01);
+    EXPECT_EQ(header[9], 0x02);
+    EXPECT_EQ(header[10], 0x00);
+    EXPECT_EQ(header[11], 0x00);
+    EXPECT_EQ(header[12], 0x00);
+    EXPECT_EQ(header[13], 0x07);
+    EXPECT_EQ(header[14], 0x0d);
+    EXPECT_EQ(header[15], 0x0a);
+}
 
-    uint32_t extractedClientId = (header[1] << 24) | (header[2] << 16) | (header[3] << 8) | header[4];
-    EXPECT_EQ(extractedClientId, clientId);
+TEST_F(PacketTest, PackBodyPacketProducesCorrectBody) {
+    Packet packet(1);
+    packet.setType(0x01);
 
-    uint64_t extractedSequence = 0;
-    for (size_t i = 0; i < 8; ++i) {
-        extractedSequence = (extractedSequence << 8) | header[5 + i];
-    }
-    EXPECT_EQ(extractedSequence, 1001);
-    EXPECT_EQ(header[13], 0x02);
+    std::vector<uint8_t> payload = {0x02, 0x04};
+    std::vector<uint8_t> body = packet.packBodyPacket(payload);
 
-    uint64_t extractedLength = 0;
-    for (size_t i = 0; i < 8; ++i) {
-        extractedLength = (extractedLength << 8) | header[14 + i];
-    }
-    EXPECT_EQ(extractedLength, 256);
+    EXPECT_EQ(body.size(), 7);
+    EXPECT_EQ(body[0], 0x02);
+    EXPECT_EQ(body[1], 0x00);
+    EXPECT_EQ(body[2], 0x00);
+    EXPECT_EQ(body[3], 0x00);
+    EXPECT_EQ(body[4], 0x04);
+    EXPECT_EQ(body[5], 0x0d);
+    EXPECT_EQ(body[6], 0x0a);
+}
+
+TEST_F(PacketTest, PackBodyFailure) {
+    Packet packet(1);
+
+    std::vector<uint8_t> payload = {0xFF, 0x04};
+    std::vector<uint8_t> body = packet.packBodyPacket(payload);
+
+    EXPECT_TRUE(body.empty());
 }
 
 int main(int argc, char **argv) {
