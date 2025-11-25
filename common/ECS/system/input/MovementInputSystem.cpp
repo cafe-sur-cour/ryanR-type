ea5391bae986ad2dfd8e63b6e29f1ddbceb6e902
eliotttesnier <eliott.tesnier@epitech.eu>
2025-11-25 11:35:30 +0100

    refactor(ECS): improve formatting and readability in MovementInputSystem and View classes

--- common/ECS/system/input/MovementInputSystem.cpp
+++ common/ECS/system/input/MovementInputSystem.cpp
@@ -6,9 +6,10 @@
 */
 
 #include "MovementInputSystem.hpp"
+#include <cmath>
+#include <memory>
 #include "../../../types/Vector2f.hpp"
 #include "../../component/tags/PlayerTag.hpp"
-#include <cmath>
 
 namespace ecs {
 
@@ -21,7 +22,11 @@ MovementInputSystem::MovementInputSystem() {
     _axisInput = math::Vector2f(0.0f, 0.0f);
 }
 
-void MovementInputSystem::update(std::shared_ptr<ResourceManager> resourceManager, std::shared_ptr<ARegistry> registry, float deltaTime) {
+void MovementInputSystem::update(
+    std::shared_ptr<ResourceManager> resourceManager,
+    std::shared_ptr<ARegistry> registry,
+    float deltaTime) {
+
     auto view = registry->view<PlayerTag>();
     math::Vector2f movementDirection = getMovementDirection();
 
@@ -43,19 +48,26 @@ void MovementInputSystem::simulateAxis(float horizontal, float vertical) {
 math::Vector2f MovementInputSystem::getMovementDirection() const {
     math::Vector2f direction(0.0f, 0.0f);
 
-    // TEMPORARY: Simulate input based on key states, will be replaced by real input system
-    if (_movementKeyStates.at(MovementKey::Left)) direction.setX(direction.getX() - 1.0f);
-    if (_movementKeyStates.at(MovementKey::Right)) direction.setX(direction.getX() + 1.0f);
-    if (_movementKeyStates.at(MovementKey::Up)) direction.setY(direction.getY() - 1.0f);
-    if (_movementKeyStates.at(MovementKey::Down)) direction.setY(direction.getY() + 1.0f);
+    // TEMPORARY: Simulate input based on key states
+    // will be replaced by real input system
+    if (_movementKeyStates.at(MovementKey::Left))
+        direction.setX(direction.getX() - 1.0f);
+    if (_movementKeyStates.at(MovementKey::Right))
+        direction.setX(direction.getX() + 1.0f);
+    if (_movementKeyStates.at(MovementKey::Up))
+        direction.setY(direction.getY() - 1.0f);
+    if (_movementKeyStates.at(MovementKey::Down))
+        direction.setY(direction.getY() + 1.0f);
 
+    // will be replaced by real input system
     if (_axisInput.getX() != 0.0f || _axisInput.getY() != 0.0f) {
         direction = _axisInput;
     }
 
     /* Normalize direction */
     if (direction.getX() != 0.0f && direction.getY() != 0.0f) {
-        float length = std::sqrt(direction.getX() * direction.getX() + direction.getY() * direction.getY());
+        float length = std::sqrt(direction.getX() * direction.getX() +
+                                  direction.getY() * direction.getY());
         if (length > 1.0f) {
             direction.setX(direction.getX() / length);
             direction.setY(direction.getY() / length);
@@ -65,13 +77,18 @@ math::Vector2f MovementInputSystem::getMovementDirection() const {
     return direction;
 }
 
-void MovementInputSystem::updateMovementIntent(std::shared_ptr<ARegistry> registry, int entityId, math::Vector2f direction) {
-    registry->registerComponent<MovementIntentComponent>();
+void MovementInputSystem::updateMovementIntent(
+    std::shared_ptr<ARegistry> registry,
+    int entityId,
+    math::Vector2f direction) {
 
-    auto movementIntent = std::make_shared<MovementIntentComponent>(direction, true);
+    registry->registerComponent<MovementIntentComponent>();
+    auto movementIntent = std::make_shared<MovementIntentComponent>(direction,
+                                                                     true);
 
     if (registry->hasComponent<MovementIntentComponent>(entityId)) {
-        auto existingIntent = registry->getComponent<MovementIntentComponent>(entityId);
+        auto existingIntent = registry->getComponent<MovementIntentComponent>(
+            entityId);
         existingIntent->setDirection(direction);
         existingIntent->setActive(true);
         existingIntent->setState(ComponentState::Temporary);
