79be7195ebe23a9f6c53ce20b07f455edcb4e3de
eliotttesnier <eliott.tesnier@epitech.eu>
2025-11-25 15:02:27 +0100

    feat(SpeedComponent): add SpeedComponent class with constructors and methods

---
+++ common/ECS/component/permanent/SpeedComponent.hpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** SpeedComponent
+*/
+
+#ifndef SPEEDCOMPONENT_HPP_
+#define SPEEDCOMPONENT_HPP_
+
+#include "../base/AComponent.hpp"
+#include "constants.hpp"
+
+namespace ecs {
+
+class SpeedComponent : public AComponent {
+    public:
+        SpeedComponent(float speed = constants::BASE_SPEED) : _speed(speed) {};
+        ~SpeedComponent() = default;
+
+        float getSpeed() const { return _speed; };
+        void setSpeed(float speed) { _speed = speed; };
+    private:
+        float _speed;
+};
+
+}  // namespace ecs
+
+#endif /* !SPEEDCOMPONENT_HPP_ */
