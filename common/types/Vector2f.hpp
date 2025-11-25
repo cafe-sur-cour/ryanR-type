befaaaaae2a0922808ad78874d34e46ff6c1b5a2
Marin <marin.lamy@epitech.eu>
2025-11-25 09:02:05 +0100

    feat(types): Add Vector2f class with basic operations and CMake integration

---
+++ common/types/Vector2f.hpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** Vector2f
+*/
+
+#ifndef VECTOR2F_HPP_
+#define VECTOR2F_HPP_
+
+class Vector2f {
+    public:
+        Vector2f(float x = 0.0f, float y = 0.0f);
+        ~Vector2f() = default;
+
+        float getX() const;
+        void setX(float x);
+        float getY() const;
+        void setY(float y);
+
+        Vector2f getVector() const;
+        Vector2f operator*(float scalar) const;
+        Vector2f operator-(Vector2f const &other) const;
+        Vector2f operator+(Vector2f const &other) const;
+        void operator=(Vector2f const &other);
+        void operator+=(Vector2f const &other);
+        void operator-=(Vector2f const &other);
+        void operator*=(float scalar);
+        void operator/=(float scalar);
+    private:
+        float _x;
+        float _y;
+};
+
+#endif /* !VECTOR2F_HPP_ */
