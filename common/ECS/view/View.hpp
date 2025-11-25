9a32223fd2e476b8ec361b9a63d3328e2a19f7dd
eliotttesnier <eliott.tesnier@epitech.eu>
2025-11-25 11:26:15 +0100

    feat(View): implement View and Group classes with iterators for ECS

---
+++ common/ECS/view/View.hpp
@@ -0,0 +1,75 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** View
+*/
+
+#ifndef VIEW_HPP_
+#define VIEW_HPP_
+
+#include <vector>
+#include <memory>
+#include <type_traits>
+
+namespace ecs {
+
+template <typename... Components>
+class View {
+    public:
+        View(std::shared_ptr<class ARegistry> registry);
+
+        class Iterator;
+
+        Iterator begin();
+        Iterator end();
+
+        class Iterator {
+            public:
+                Iterator(std::shared_ptr<class ARegistry> registry, size_t entityId, size_t maxEntityId);
+                bool operator!=(const Iterator& other) const;
+                Iterator& operator++();
+                size_t operator*() const;
+
+            private:
+                bool hasAllComponents() const;
+                std::shared_ptr<class ARegistry> _registry;
+                size_t _entityId;
+                size_t _maxEntityId;
+        };
+
+    private:
+        std::shared_ptr<class ARegistry> _registry;
+};
+
+template <typename... Components>
+class Group {
+    public:
+        Group(std::shared_ptr<class ARegistry> registry);
+
+        class Iterator;
+
+        Iterator begin();
+        Iterator end();
+
+        class Iterator {
+            public:
+                Iterator(std::shared_ptr<class ARegistry> registry, size_t entityId, size_t maxEntityId);
+                bool operator!=(const Iterator& other) const;
+                Iterator& operator++();
+                size_t operator*() const;
+
+            private:
+                bool hasAllComponents() const;
+                std::shared_ptr<class ARegistry> _registry;
+                size_t _entityId;
+                size_t _maxEntityId;
+        };
+
+    private:
+        std::shared_ptr<class ARegistry> _registry;
+};
+
+}  // namespace ecs
+
+#endif /* !VIEW_HPP_ */
