979a79f328491c44e062a460e9906fda628106ed
Marin <marin.lamy@epitech.eu>
2025-11-26 11:48:53 +0100

    feat(SfmlWindow): add drawSprite method for ECS sprite rendering and update IWindow interface

--- client/graphicals/SfmlWindow.cpp
+++ client/graphicals/SfmlWindow.cpp
@@ -118,3 +118,22 @@ std::string SfmlWindow::getFont() const {
 std::shared_ptr<sf::RenderWindow> SfmlWindow::getSfmlWindow() {
     return _window;
 }
+
+void SfmlWindow::drawSprite(const std::string& texturePath, float x, float y, float scaleX, float scaleY) {
+    auto it = _textureCache.find(texturePath);
+    std::shared_ptr<sf::Texture> texture;
+    if (it == _textureCache.end()) {
+        texture = std::make_shared<sf::Texture>();
+        if (!texture->loadFromFile(texturePath)) {
+            drawRectangle({255, 0, 0}, {static_cast<size_t>(x), static_cast<size_t>(y)}, {50, 50});
+            return;
+        }
+        _textureCache[texturePath] = texture;
+    } else {
+        texture = it->second;
+    }
+    sf::Sprite sprite(*texture);
+    sprite.setPosition(sf::Vector2f(x, y));
+    sprite.setScale(sf::Vector2f(scaleX, scaleY));
+    _window->draw(sprite);
+}
