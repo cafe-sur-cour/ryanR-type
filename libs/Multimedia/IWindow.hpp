d739fef7be37d6212e510bee4dd8b9e270b45be5
albanrss <alban.roussee@epitech.eu>
2025-11-27 12:10:32 +0100

    feat(multimedia): add the multimedia lib, that handles sfml window and events

---
+++ libs/Multimedia/IWindow.hpp
@@ -0,0 +1,47 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** IWindow
+*/
+
+#ifndef IWINDOW_HPP_
+#define IWINDOW_HPP_
+
+#include <string>
+#include <utility>
+#include "../../common/types/FRect.hpp"
+
+namespace gfx {
+
+struct color_t {
+    uint8_t r;
+    uint8_t g;
+    uint8_t b;
+};
+
+class IWindow {
+    public:
+        virtual ~IWindow() = default;
+
+        virtual void init() = 0;
+        virtual void display() = 0;
+        virtual void closeWindow() = 0;
+        virtual bool isOpen() = 0;
+        virtual void clear() = 0;
+        virtual void resizeWindow(size_t x, size_t y) = 0;
+
+        virtual void drawSprite(std::string asset, color_t color, std::pair<size_t, size_t> position) = 0;
+        virtual void drawText(std::string text, color_t color, std::pair<size_t, size_t> position, const std::string& fontPath) = 0;
+        virtual void drawRectangle(color_t color, std::pair<size_t, size_t> position, std::pair<size_t, size_t> size) = 0;
+
+        virtual bool isMouseOver(std::pair<size_t, size_t> position, std::pair<size_t, size_t> size) = 0;
+        virtual std::pair<int, int> getWindowSize() = 0;
+
+        virtual void drawSprite(const std::string& texturePath, float x, float y, float scaleX = 1.0f, float scaleY = 1.0f) = 0;
+        virtual void drawSprite(const std::string& texturePath, float x, float y, const math::FRect frameRect, float scaleX = 1.0f, float scaleY = 1.0f) = 0;
+};
+
+} // namespace gfx
+
+#endif /* !IWINDOW_HPP_ */
---
+++ libs/Multimedia/TextureManager.cpp
@@ -0,0 +1,56 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** TextureManager
+*/
+
+#include "TextureManager.hpp"
+#include <iostream>
+#include <memory>
+#include <string>
+namespace gfx {
+
+TextureManager::TextureManager(
+    std::shared_ptr<assets::AssetManager> assetManager)
+    : _assetManager(assetManager) {}
+
+std::shared_ptr<sf::Texture> TextureManager::loadTexture
+(const std::string& path) {
+    if (_failedTextures.find(path) != _failedTextures.end())
+        return nullptr;
+    auto it = _textureCache.find(path);
+    if (it != _textureCache.end())
+        return it->second;
+
+    std::string normalizedPath = path;
+    if (normalizedPath.find("./assets/") == 0)
+        normalizedPath = normalizedPath.substr(9);
+    if (normalizedPath.find("assets/") == 0)
+        normalizedPath = normalizedPath.substr(7);
+
+    auto asset = _assetManager->getAsset(normalizedPath);
+    if (!asset) {
+        std::cerr << "[TextureManager] Failed to load asset: " <<
+        normalizedPath << std::endl;
+        _failedTextures.insert(path);
+        return nullptr;
+    }
+
+    auto texture = std::make_shared<sf::Texture>();
+    if (!texture->loadFromMemory(asset->data.data(), asset->size)) {
+        std::cerr << "[TextureManager] Failed to load texture from memory: " <<
+        path << std::endl;
+        _failedTextures.insert(path);
+        return nullptr;
+    }
+    _textureCache[path] = texture;
+    return texture;
+}
+
+void TextureManager::clearCache() {
+    _textureCache.clear();
+    _failedTextures.clear();
+}
+
+}  // namespace gfx
