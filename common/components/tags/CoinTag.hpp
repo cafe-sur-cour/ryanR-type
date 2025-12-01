13695d35cc6efcfe674395eabf0fd8c65b1db3d3
albanrss <alban.roussee@epitech.eu>
2025-12-01 18:44:47 +0100

    feat(tag): add CoinTag component for coin identification in ECS

---
+++ common/components/tags/CoinTag.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** CoinTag
+*/
+
+#ifndef COINTAG_HPP_
+#define COINTAG_HPP_
+
+#include "../base/AComponent.hpp"
+
+namespace ecs {
+
+class CoinTag : public AComponent {
+    public:
+        CoinTag() = default;
+        ~CoinTag() = default;
+};
+
+} // namespace ecs
+
+#endif /* !COINTAG_HPP_ */
