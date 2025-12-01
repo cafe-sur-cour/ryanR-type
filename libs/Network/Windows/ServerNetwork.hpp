9141278b4c46944512cc3e9b425c90515af97f54
Albane Merian <albane.merian@epitech.eu>
2025-12-01 13:58:31 +0100

    feat(windwos): Define the windows class for the windows network library

---
+++ libs/Network/Windows/ServerNetwork.hpp
@@ -0,0 +1,50 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** ServerNetwork
+*/
+
+#ifndef SERVERNETWORK_HPP_
+#define SERVERNETWORK_HPP_
+
+#include <asio.hpp>
+#include <unordered_map>
+#include <memory>
+#include <queue>
+#include <functional>
+#include "../ANetwork.hpp"
+#include "../../Buffer/IBuffer.hpp"
+#include "../../Packet/IPacketManager.hpp"
+
+namespace net {
+
+class ServerNetwork :  public ANetwork {
+    public:
+        ServerNetwork();
+        ~ServerNetwork() override;
+
+        void init(int port) override;
+        void stop() override;
+
+        int acceptConnection() override;
+
+        void sendTo(int connectionId, const pm::IPacketManager &packet) override;
+        void broadcast(const pm::IPacketManager &packet) override;
+        bool hasIncomingData() const override;
+        std::shared_ptr<pm::IPacketManager> receiveFrom(const int &connectionId) override;
+
+    private:
+        std::queue<std::pair<int, std::shared_ptr<pm::IPacketManager>>> _incomingPackets;
+        int _nextClientId;
+        int _port;
+};
+
+} // namespace net
+
+extern "C" {
+    void *createNetworkInstance();
+    int getType();
+}
+
+#endif /* !SERVERNETWORK_HPP_ */
