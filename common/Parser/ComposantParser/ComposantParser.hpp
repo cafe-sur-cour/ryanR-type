271bebb36a65dc5e2ba8810d754bbdf742ed8316
Marin <marin.lamy@epitech.eu>
2025-12-01 19:23:45 +0100

    feat(parser): add ComposantParser class for component parsing from JSON

---
+++ common/Parser/ComposantParser/ComposantParser.hpp
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** ComposantParser
+*/
+
+#ifndef COMPOSANTPARSER_HPP_
+#define COMPOSANTPARSER_HPP_
+
+#include <string>
+#include <memory>
+#include <map>
+#include <typeindex>
+#include "../../components/base/IComponent.hpp"
+#include "../ParserParam.hpp"
+#include <nlohmann/json.hpp>
+
+class ComposantParser {
+    public:
+        ComposantParser(const std::map<std::string, std::pair<std::type_index, std::vector<Field>>>& componentDefinitions, const std::map<std::type_index, ComponentCreator>& componentCreators);
+        ~ComposantParser();
+
+        std::pair<std::shared_ptr<ecs::IComponent>, std::type_index> parseComponent(const std::string& componentName, const nlohmann::json& componentData);
+
+    protected:
+    private:
+        std::shared_ptr<FieldValue> parseFieldValue(const nlohmann::json& jsonValue, FieldType type);
+        const std::map<std::string, std::pair<std::type_index, std::vector<Field>>>& _componentDefinitions;
+        const std::map<std::type_index, ComponentCreator>& _componentCreators;
+};
+
+#endif /* !COMPOSANTPARSER_HPP_ */
