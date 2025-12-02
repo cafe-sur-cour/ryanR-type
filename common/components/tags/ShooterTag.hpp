7fc4f6b0a924608ebd56fdb48f3674d233563c48
albanrss <alban.roussee@epitech.eu>
2025-12-02 11:03:39 +0100

    feat(component): add ShooterTag component to ECS

---
+++ common/components/tags/ShooterTag.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** ShooterTag
+*/
+
+#ifndef SHOOTERTAG_HPP_
+#define SHOOTERTAG_HPP_
+
+#include "../base/AComponent.hpp"
+
+namespace ecs {
+
+class ShooterTag : public AComponent {
+    public:
+        ShooterTag() = default;
+        ~ShooterTag() = default;
+};
+
+}
+
+#endif /* !SHOOTERTAG_HPP_ */
