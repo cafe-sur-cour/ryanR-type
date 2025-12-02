b6f54689714ce4683737a4f9350bebbc6f134174
Marin <marin.lamy@epitech.eu>
2025-12-02 11:49:42 +0100

    style(parser): improve code readability by formatting and organizing includes across multiple files

--- common/Prefab/ParsedEntityPrefab.cpp
+++ common/Prefab/ParsedEntityPrefab.cpp
@@ -5,23 +5,30 @@
 ** ParsedEntityPrefab
 */
 
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <memory>
 #include "ParsedEntityPrefab.hpp"
 #include "../Parser/ParserParam.hpp"
 #include "../Error/ParserError.hpp"
-#include <iostream>
 
-ParsedEntityPrefab::ParsedEntityPrefab(const std::string& name, const std::map<std::type_index, ComponentAdder>& adders) 
+ParsedEntityPrefab::ParsedEntityPrefab(const std::string& name, const std::map<std::type_index,
+    ComponentAdder>& adders)
     : _name(name), _componentAdders(adders) {
 }
 
 ParsedEntityPrefab::~ParsedEntityPrefab() {
 }
 
-void ParsedEntityPrefab::addComponent(std::shared_ptr<ecs::IComponent> component, std::type_index typeIndex) {
+void ParsedEntityPrefab::addComponent(std::shared_ptr<ecs::IComponent> component,
+    std::type_index typeIndex) {
     _components.push_back({component, typeIndex});
 }
 
-const std::vector<std::shared_ptr<ecs::IComponent>>& ParsedEntityPrefab::getComponents() const {
+const std::vector<std::shared_ptr<ecs::IComponent>>& ParsedEntityPrefab::getComponents()
+    const {
     static std::vector<std::shared_ptr<ecs::IComponent>> temp;
     temp.clear();
     for (const auto& [comp, _] : _components) {
@@ -34,7 +41,8 @@ std::string ParsedEntityPrefab::getName() const {
     return _name;
 }
 
-ecs::Entity ParsedEntityPrefab::instantiate(const std::shared_ptr<ecs::Registry>& registry) {
+ecs::Entity ParsedEntityPrefab::instantiate(
+    const std::shared_ptr<ecs::Registry>& registry) {
     ecs::Entity entity = registry->createEntity();
 
     for (const auto& [component, typeIndex] : _components) {
@@ -42,7 +50,8 @@ ecs::Entity ParsedEntityPrefab::instantiate(const std::shared_ptr<ecs::Registry>
         if (it != _componentAdders.end()) {
             it->second(registry, entity, component);
         } else {
-            throw err::ParserError(std::string("Unknown component type: ") + typeIndex.name(), err::ParserError::UNKNOWN);
+            throw err::ParserError(std::string("Unknown component type: ") +
+            typeIndex.name(), err::ParserError::UNKNOWN);
         }
     }
     return entity;
