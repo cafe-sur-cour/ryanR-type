6a1dc68932c05885bb6b342d39007bfe0430faef
albanrss <alban.roussee@epitech.eu>
2025-12-02 13:39:33 +0100

    feat(system): add ShootingSystem for managing shooting mechanics

---
+++ common/systems/shooting/ShootingSystem.hpp
@@ -0,0 +1,50 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** ShootingSystem
+*/
+
+#ifndef SHOOTINGSYSTEM_HPP_
+#define SHOOTINGSYSTEM_HPP_
+
+#include "../base/ASystem.hpp"
+#include "../../components/base/IComponent.hpp"
+#include "../../components/temporary/ShootIntentComponent.hpp"
+#include "../../components/permanent/ShootingStatsComponent.hpp"
+#include "../../components/permanent/TransformComponent.hpp"
+#include "../../components/permanent/VelocityComponent.hpp"
+#include "../../types/Vector2f.hpp"
+#include <cmath>
+
+namespace ecs {
+
+class ShootingSystem : public ASystem {
+    public:
+        ShootingSystem();
+        ~ShootingSystem() = default;
+
+        void update(
+            std::shared_ptr<ResourceManager> resourceManager,
+            std::shared_ptr<Registry> registry,
+            float deltaTime
+        ) override;
+
+    private:
+        void spawnProjectile(
+            std::shared_ptr<Registry> registry,
+            std::shared_ptr<IPrefab> projectilePrefab,
+            const math::Vector2f &position,
+            float angle,
+            float speed
+        );
+
+        math::Vector2f calculateProjectileVelocity(
+            float angle,
+            float speed
+        );
+};
+
+}  // namespace ecs
+
+#endif /* !SHOOTINGSYSTEM_HPP_ */
