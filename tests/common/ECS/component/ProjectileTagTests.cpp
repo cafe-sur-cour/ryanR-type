db8b2e7b8216d121bbfe5da5e0d19251e4017425
eliotttesnier <eliott.tesnier@epitech.eu>
2025-12-02 11:45:23 +0100

    refactor(tests): remove redundant state inheritance tests from component test files

--- tests/common/ECS/component/ProjectileTagTests.cpp
+++ tests/common/ECS/component/ProjectileTagTests.cpp
@@ -17,19 +17,6 @@ TEST(ProjectileTagTest, DefaultConstructor) {
     SUCCEED();
 }
 
-TEST(ProjectileTagTest, InheritsFromAComponent) {
-    ProjectileTag tag;
-    EXPECT_EQ(tag.getState(), ComponentState::Permanent);
-}
-
-TEST(ProjectileTagTest, CanChangeState) {
-    ProjectileTag tag;
-    tag.setState(ComponentState::Temporary);
-    EXPECT_EQ(tag.getState(), ComponentState::Temporary);
-    tag.setState(ComponentState::Processed);
-    EXPECT_EQ(tag.getState(), ComponentState::Processed);
-}
-
 int main(int argc, char **argv) {
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
