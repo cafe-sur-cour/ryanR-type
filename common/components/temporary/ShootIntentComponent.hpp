17baebeda2c4e21cb0b8ec702aa4737251ab1f31
albanrss <alban.roussee@epitech.eu>
2025-12-02 11:02:45 +0100

    upt(component): remove direction handling from ShootIntentComponent

--- common/components/temporary/ShootIntentComponent.hpp
+++ common/components/temporary/ShootIntentComponent.hpp
@@ -16,21 +16,16 @@ namespace ecs {
 class ShootIntentComponent : public AComponent {
     public:
         ShootIntentComponent(
-            const math::Vector2f &direction = math::Vector2f(0.0f, 0.0f),
             const math::Vector2f &position = math::Vector2f(0.0f, 0.0f)
-        ) : _direction(direction), _position(position) {
+        ) : _position(position) {
             _state = Temporary;
         };
         ~ShootIntentComponent() = default;
-
-        math::Vector2f getDirection() const { return _direction; };
-        void setDirection(const math::Vector2f &direction) { _direction = direction; };
 
         math::Vector2f getPosition() const { return _position; };
         void setPosition(const math::Vector2f &position) { _position = position; };
 
     private:
-        math::Vector2f _direction;
         math::Vector2f _position;
 };
 
