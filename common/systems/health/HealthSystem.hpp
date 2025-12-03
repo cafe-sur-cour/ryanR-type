3ac04cddc040dd14dda84a2c68b9815d2c7abd8d
albanrss <alban.roussee@epitech.eu>
2025-12-03 14:31:43 +0100

    feat(HealthSystem): implement HealthSystem for managing entity health and destruction

---
+++ common/systems/health/HealthSystem.hpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** HealthSystem
+*/
+
+#ifndef HEALTHSYSTEM_HPP_
+#define HEALTHSYSTEM_HPP_
+
+#include "../base/ASystem.hpp"
+#include <memory>
+
+namespace ecs {
+
+class HealthSystem : public ASystem {
+    public:
+        HealthSystem();
+        ~HealthSystem() override = default;
+
+        void update(
+            std::shared_ptr<ResourceManager> resourceManager,
+            std::shared_ptr<Registry> registry,
+            float deltaTime
+        ) override;
+};
+
+}
+
+#endif /* !HEALTHSYSTEM_HPP_ */
