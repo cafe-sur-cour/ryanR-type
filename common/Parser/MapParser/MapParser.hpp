e19622039eb3e29ea6760058550afa402e35f682
Marin <marin.lamy@epitech.eu>
2025-12-03 14:44:29 +0100

    feat(MapParser): implement MapParser class for map file parsing and entity creation

---
+++ common/Parser/MapParser/MapParser.hpp
@@ -0,0 +1,39 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** MapParser
+*/
+
+#ifndef MAPPARSER_HPP_
+#define MAPPARSER_HPP_
+
+#include <string>
+#include <memory>
+#include <nlohmann/json.hpp>
+#include "../../ECS/entity/registry/Registry.hpp"
+#include "../../Prefab/entityPrefabManager/EntityPrefabManager.hpp"
+#include "../../constants.hpp"
+#include "../../types/Vector2f.hpp"
+
+class MapParser {
+    public:
+        MapParser(std::shared_ptr<EntityPrefabManager> prefabManager,
+            std::shared_ptr<ecs::Registry> registry);
+        ~MapParser();
+
+        void parseMapFromFile(const std::string& filePath);
+        void parseMap(const nlohmann::json& mapJson);
+    protected:
+    private:
+        std::shared_ptr<EntityPrefabManager> _prefabManager;
+        std::shared_ptr<ecs::Registry> _registry;
+        void createBackgroundEntity(const std::string& entityName, float scrollSpeed);
+        void parseMapGrid(const nlohmann::json& legend, const nlohmann::json& mapGrid,
+            float tileWidth, float tileHeight);
+        void parseWaves(const nlohmann::json& waves, float tileWidth);
+        ecs::Entity createEntityFromPrefab(const std::string& prefabName,
+            float x, float y);
+};
+
+#endif /* !MAPPARSER_HPP_ */
