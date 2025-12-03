b9e18170cbe24ce042498762687f2e37c1c14524
albanrss <alban.roussee@epitech.eu>
2025-12-03 12:23:45 +0100

    feat(LifetimeComponent): add new LifetimeComponent class for managing component lifetimes

---
+++ common/components/permanent/LifetimeComponent.hpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** LifetimeComponent
+*/
+
+#ifndef LIFETIMECOMPONENT_HPP_
+#define LIFETIMECOMPONENT_HPP_
+
+#include "../base/AComponent.hpp"
+
+namespace ecs {
+
+class LifetimeComponent : public AComponent {
+    public:
+        LifetimeComponent(float lifetime = 0.0f) : _lifetime(lifetime) {};
+        ~LifetimeComponent() = default;
+
+        float getLifetime() const { return _lifetime; };
+        void setLifetime(float lifetime) { _lifetime = lifetime; };
+
+    private:
+        float _lifetime;
+};
+
+} // ecs
+
+
+#endif /* !LIFETIMECOMPONENT_HPP_ */
