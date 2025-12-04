098ff206b62a330bb9c67d684ce0d36958d7d112
Albane Merian <albane.merian@epitech.eu>
2025-12-04 19:44:34 +0100

    feat(debug): Add a debug class with different levels and type to allow more information of the client

---
+++ common/debug.hpp
@@ -0,0 +1,35 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** debug
+*/
+
+#ifndef DEBUG_HPP_
+#define DEBUG_HPP_
+
+#include <string>
+
+namespace debug {
+
+enum class debugType {
+    NETWORK,
+    ECS,
+    CORE,
+};
+
+enum class debugLevel {
+    INFO,
+    WARNING,
+    ERROR,
+};
+
+class Debug {
+    public:
+        virtual ~Debug() = default;
+        static void printDebug(const bool isDebug, const std::string &message, const debugType type, const debugLevel level);
+};
+
+}
+
+#endif /* !DEBUG_HPP_ */
