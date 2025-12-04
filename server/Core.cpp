a33b965f8c591a1f24cff9af9500d50c4ae9267d
Matisse-M <matisse.marsac@epitech.eu>
2025-12-04 14:47:58 +0100

    feat(server): move server in a dedicated thread

--- server/Core.cpp
+++ server/Core.cpp
@@ -18,11 +18,16 @@ Core::Core() {
 }
 
 Core::~Core() {
+    if (this->_serverThread.joinable()) {
+        if (this->_server != nullptr && this->_server->getState() == 1) {
+            this->_server->stop();
+        }
+        this->_serverThread.join();
+    }
     if (this->_resourceManager != nullptr) {
         this->_resourceManager.reset();
     }
     if (this->_server != nullptr) {
-        this->_server->stop();
         this->_server.reset();
     }
     if (this->_utils != nullptr) {
@@ -32,11 +37,13 @@ Core::~Core() {
 
 void Core::init() {
     this->_resourceManager = initResourcesManager();
-    this->_server->init();
 }
 
 void Core::loop() {
-    this->_server->start();
+    this->_serverThread = std::thread([this]() {
+        this->_server->init();
+        this->_server->start();
+    });
 }
 
 std::shared_ptr<rserv::ServerConfig> Core::getConfig() const {
