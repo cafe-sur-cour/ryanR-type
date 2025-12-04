c4546f4f2578bb104739a8231cabe6e47b94d474
eliotttesnier <eliott.tesnier@epitech.eu>
2025-12-04 21:30:55 +0100

    feat(tag_registry): refactor TagRegistry to initialize tags and update singleton access method

--- common/systems/interactions/InteractionSystem.cpp
+++ common/systems/interactions/InteractionSystem.cpp
@@ -7,47 +7,16 @@
 
 #include "InteractionSystem.hpp"
 #include <memory>
-#include <unordered_map>
-#include <functional>
 #include <string>
 #include "../../ECS/entity/registry/Registry.hpp"
 #include "../../components/temporary/TriggerIntentComponent.hpp"
 #include "../../components/permanent/InteractionConfigComponent.hpp"
-#include "../../components/tags/MobTag.hpp"
-#include "../../components/tags/ProjectileTag.hpp"
-#include "../../components/tags/PlayerTag.hpp"
 #include "../../Parser/Action/ActionFactory.hpp"
-#include "../../constants.hpp"
+#include "../../Parser/Action/TagRegistry.hpp"
 
 namespace ecs {
 
 InteractionSystem::InteractionSystem() {
-}
-
-bool InteractionSystem::entityHasTag(
-    std::shared_ptr<Registry> registry,
-    ecs::Entity entity,
-    const std::string& tagName
-) {
-    static const std::unordered_map<std::string,
-        std::function<bool(std::shared_ptr<Registry>, ecs::Entity)>> tagCheckers = {
-        {constants::MOBTAG,
-            [](std::shared_ptr<Registry> reg, ecs::Entity ent)
-                { return reg->hasComponent<MobTag>(ent); }},
-        {constants::PROJECTILETAG,
-            [](std::shared_ptr<Registry> reg, ecs::Entity ent)
-                { return reg->hasComponent<ProjectileTag>(ent); }},
-        {constants::PLAYERTAG,
-            [](std::shared_ptr<Registry> reg, ecs::Entity ent)
-                { return reg->hasComponent<PlayerTag>(ent); }},
-    };
-
-    auto it = tagCheckers.find(tagName);
-    if (it != tagCheckers.end()) {
-        return it->second(registry, entity);
-    }
-
-    return false;
 }
 
 void InteractionSystem::update(
@@ -78,7 +47,7 @@ void InteractionSystem::update(
         for (const auto& mapping : interactionConfig->getMappings()) {
             bool hasRequiredTag = false;
             for (const auto& targetTag : mapping.targetTags) {
-                if (entityHasTag(registry, otherEntity, targetTag)) {
+                if (TagRegistry::getInstance().hasTag(registry, otherEntity, targetTag)) {
                     hasRequiredTag = true;
                     break;
                 }
