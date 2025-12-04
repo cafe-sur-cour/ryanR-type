04d88695d26b7e50ea960ee883228640fe9279b8
Marin <marin.lamy@epitech.eu>
2025-12-04 19:47:58 +0100

    feat(server): enhance AGameState to support ECS systems and resource management

--- client/gsm/states/base/AGameState.cpp
+++ client/gsm/states/base/AGameState.cpp
@@ -1,9 +1,12 @@
 #include "AGameState.hpp"
+#include <stdexcept>
+#include "../../../../common/systems/systemManager/ISystemManager.hpp"
 
 namespace gsm {
 
-AGameState::AGameState(std::shared_ptr<IGameStateMachine> gsm) {
+AGameState::AGameState(std::shared_ptr<IGameStateMachine> gsm, std::shared_ptr<ResourceManager> resourceManager) {
     _gsm = gsm;
+    _resourceManager = resourceManager;
 }
 
 void AGameState::enter() {
@@ -14,6 +17,18 @@ void AGameState::update(float deltaTime) {
 }
 
 void AGameState::exit() {
+}
+
+void AGameState::addSystem(std::shared_ptr<ecs::ISystem> system) {
+    if (_resourceManager->has<ecs::ISystemManager>())
+        _resourceManager->get<ecs::ISystemManager>()->addSystem(system);
+    else
+        throw std::runtime_error("AGameState::addSystem: ISystemManager not found in ResourceManager");
+    _systems.push_back(system);
+}
+
+std::vector<std::shared_ptr<ecs::ISystem>> AGameState::getSystems() const {
+    return _systems;
 }
 
 }  // namespace gsm
