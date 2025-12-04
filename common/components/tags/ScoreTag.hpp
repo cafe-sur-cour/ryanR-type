03739f0cfc334f858a8caabfeedfbeaa014e953f
Marin <marin.lamy@epitech.eu>
2025-12-04 09:40:09 +0100

    feat(score): add ScoreComponent, ScoreTag, and ScoreIntentComponent for score management

---
+++ common/components/tags/ScoreTag.hpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** ScoreTag
+*/
+
+#ifndef SCORETAG_HPP_
+#define SCORETAG_HPP_
+
+#include "../base/AComponent.hpp"
+
+namespace ecs {
+
+class ScoreTag : public AComponent {
+    public:
+        ScoreTag() {};
+        ~ScoreTag() {};
+
+    protected:
+    private:
+};
+
+}  // namespace ecs
+
+#endif /* !SCORETAG_HPP_ */
