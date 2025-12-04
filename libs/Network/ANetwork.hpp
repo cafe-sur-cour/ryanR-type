24d66cc0db70dc22412bc470de16d3f267cdc82a
Matisse-M <matisse.marsac@epitech.eu>
2025-12-04 10:04:42 +0100

    refactor(network): remove useless functions in network shared kib

--- libs/Network/ANetwork.hpp
+++ libs/Network/ANetwork.hpp
@@ -27,21 +27,16 @@ class ANetwork : public INetwork {
         virtual void broadcast(std::vector<uint8_t> data) override = 0;
         virtual bool hasIncomingData() const override = 0;
         virtual std::vector<uint8_t> receiveFrom(const uint8_t &connectionId) override = 0;
+        virtual std::pair<asio::ip::udp::endpoint, std::vector<uint8_t>> receiveAny() override = 0;
 
         ConnectionState getConnectionState() const override;
         void setConnectionState(ConnectionState state) override;
-        virtual std::pair<asio::ip::udp::endpoint, std::vector<uint8_t>> receiveAny() override = 0;
-
         void setConnectionCallback(std::function<void(int)> onConnect) override;
         void setDisconnectionCallback(std::function<void(int)> onDisconnect) override;
-        std::vector<uint8_t> getActiveConnections() const override;
-        size_t getConnectionCount() const override;
-        void closeConnection(uint8_t connectionId) override;
 
     protected:
         std::shared_ptr<asio::io_context> _ioContext;
         std::shared_ptr<asio::ip::udp::socket> _socket;
-        std::unordered_map<uint8_t, asio::ip::udp::endpoint> _clients;
         std::function<void(int)> _onConnectCallback;
         std::function<void(int)> _onDisconnectCallback;
         bool _isRunning;
