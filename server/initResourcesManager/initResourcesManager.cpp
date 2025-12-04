84df74da82b914b6d3fbadc776f20485a10e0c40
Marin <marin.lamy@epitech.eu>
2025-12-04 19:48:43 +0100

    feat(server): integrate GameStateMachine and enhance resource management in initResourcesManager

--- server/initResourcesManager/initResourcesManager.cpp
+++ server/initResourcesManager/initResourcesManager.cpp
@@ -7,23 +7,41 @@
 
 #include <memory>
 #include <iostream>
+#include <unordered_map>
+#include <string>
 #include "../../common/resourceManager/ResourceManager.hpp"
+#include "../../common/Prefab/entityPrefabManager/EntityPrefabManager.hpp"
 #include "ServerInputProvider.hpp"
 #include "../ServerConfig.hpp"
+#include "../../common/ECS/entity/registry/Registry.hpp"
+#include "../../common/Parser/Parser.hpp"
+#include "../../common/systems/systemManager/ASystemManager.hpp"
+#include "../gsm/machine/GameStateMachine.hpp"
 #include "initResourcesManager.hpp"
 
-std::shared_ptr<ResourceManager> initResourcesManager() {
+std::shared_ptr<ResourceManager> initResourcesManager(
+    std::shared_ptr<rserv::Server> server,
+    std::shared_ptr<ecs::Registry> registry,
+    std::shared_ptr<Parser> parser,
+    std::shared_ptr<ecs::ISystemManager> systemsManager,
+    std::shared_ptr<gsm::GameStateMachine> gameStateMachine,
+    std::shared_ptr<ecs::IInputProvider> inputProvider
+) {
     std::shared_ptr<ResourceManager> resourceManager =
         std::make_shared<ResourceManager>();
 
-    // Initialize and load resources here
-    // exemple resourceManager->
-    // add(std::make_shared<rserv::ServerConfig>(8080));
-    // networking
-    // etc.
-
-    auto inputProvider = std::make_shared<ecs::ServerInputProvider>();
     resourceManager->add<ecs::IInputProvider>(inputProvider);
+
+    auto entityPrefabManager = std::make_shared<EntityPrefabManager>();
+    resourceManager->add<EntityPrefabManager>(entityPrefabManager);
+
+    resourceManager->add<rserv::Server>(server);
+    resourceManager->add<rserv::ServerConfig>(server->getConfig());
+
+    resourceManager->add<ecs::Registry>(registry);
+    resourceManager->add<Parser>(parser);
+    resourceManager->add<ecs::ISystemManager>(systemsManager);
+    resourceManager->add<gsm::GameStateMachine>(gameStateMachine);
 
     return resourceManager;
 }
