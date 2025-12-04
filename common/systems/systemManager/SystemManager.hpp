1e5950bf305c7b3c0f9f34048d8660351ebc7d0d
Marin <marin.lamy@epitech.eu>
2025-12-04 19:49:30 +0100

    feat(server): add SystemManager class and update CMake configurations for ECS integration

---
+++ common/systems/systemManager/SystemManager.hpp
@@ -0,0 +1,26 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** SystemManager
+*/
+
+#ifndef SYSTEMMANAGER_HPP_
+#define SYSTEMMANAGER_HPP_
+
+#include "ASystemManager.hpp"
+
+namespace ecs {
+
+class SystemManager : public ASystemManager {
+    public:
+        SystemManager();
+        ~SystemManager() = default;
+
+    protected:
+    private:
+};
+
+}  // namespace ecs
+
+#endif /* !SYSTEMMANAGER_HPP_ */
