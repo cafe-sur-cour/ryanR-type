9a1163d174144b098b8ccc1eb90bc37b415b5607
eliotttesnier <eliott.tesnier@epitech.eu>
2025-12-04 11:31:07 +0100

    feat(gamezone): add GameZoneComponent and integrate into MapParser and DevState

--- common/Parser/MapParser/MapParser.hpp
+++ common/Parser/MapParser/MapParser.hpp
@@ -29,6 +29,7 @@ class MapParser {
         std::shared_ptr<EntityPrefabManager> _prefabManager;
         std::shared_ptr<ecs::Registry> _registry;
         void createBackgroundEntity(const std::string& entityName);
+        void createGameZoneEntity(float scrollSpeed);
         void parseMapGrid(const nlohmann::json& legend, const nlohmann::json& mapGrid,
             float tileWidth, float tileHeight);
         void parseWaves(const nlohmann::json& waves, float tileWidth);
