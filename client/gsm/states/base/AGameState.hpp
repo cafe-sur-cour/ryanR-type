d792f6cd015515c83e401ed32b9b6de3a58164a2
Marin <marin.lamy@epitech.eu>
2025-12-04 19:47:58 +0100

    feat(server): enhance AGameState to support ECS systems and resource management

--- client/gsm/states/base/AGameState.hpp
+++ client/gsm/states/base/AGameState.hpp
@@ -1,20 +1,25 @@
 #pragma once
 
 #include "../../../../common/gsm/IGameState.hpp"
+#include "../../../../common/resourceManager/ResourceManager.hpp"
 
 namespace gsm {
 
 class AGameState : public IGameState {
 public:
-    AGameState(std::shared_ptr<IGameStateMachine> gsm);
+    AGameState(std::shared_ptr<IGameStateMachine> gsm, std::shared_ptr<ResourceManager> resourceManager);
     ~AGameState() override = default;
 
     void enter() override;
     void update(float deltaTime) override;
     void exit() override;
+    void addSystem(std::shared_ptr<ecs::ISystem> system) override;
+    std::vector<std::shared_ptr<ecs::ISystem>> getSystems() const override;
 
 protected:
     std::shared_ptr<IGameStateMachine> _gsm;
+    std::shared_ptr<ResourceManager> _resourceManager;
+    std::vector<std::shared_ptr<ecs::ISystem>> _systems;
 };
 
 } // namespace gsm
--- server/gsm/states/AGameState.cpp
+++ server/gsm/states/AGameState.cpp
@@ -1,19 +1,38 @@
 #include "AGameState.hpp"
+#include "../../../common/systems/systemManager/ISystemManager.hpp"
+#include "../../../common/ECS/entity/registry/Registry.hpp"
 
 namespace gsm {
 
-AGameState::AGameState(std::shared_ptr<IGameStateMachine> gsm) {
+AGameState::AGameState(std::shared_ptr<IGameStateMachine> gsm, std::shared_ptr<ResourceManager> resourceManager) {
     _gsm = gsm;
+    _resourceManager = resourceManager;
 }
 
 void AGameState::enter() {
 }
 
 void AGameState::update(float deltaTime) {
-    (void)deltaTime;
+    auto systemsManager = _resourceManager->get<ecs::ISystemManager>();
+    auto registry = _resourceManager->get<ecs::Registry>();
+    if (systemsManager && registry) {
+        systemsManager->updateAllSystems(_resourceManager, registry, deltaTime);
+    }
 }
 
 void AGameState::exit() {
+}
+
+void AGameState::addSystem(std::shared_ptr<ecs::ISystem> system) {
+    if (_resourceManager->has<ecs::ISystemManager>())
+        _resourceManager->get<ecs::ISystemManager>()->addSystem(system);
+    else
+        throw std::runtime_error("AGameState::addSystem: ISystemManager not found in ResourceManager");
+    _systems.push_back(system);
+}
+
+std::vector<std::shared_ptr<ecs::ISystem>> AGameState::getSystems() const {
+    return _systems;
 }
 
 }  // namespace gsm
