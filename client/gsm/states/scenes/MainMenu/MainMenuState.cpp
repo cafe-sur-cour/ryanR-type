026e6b57312dcec3baf5513865c05212166eb3dc
eliotttesnier <eliott.tesnier@epitech.eu>
2025-12-05 15:50:27 +0100

    feat(MainMenu): update button positions and sizes for improved layout

--- client/gsm/states/scenes/MainMenu/MainMenuState.cpp
+++ client/gsm/states/scenes/MainMenu/MainMenuState.cpp
@@ -32,8 +32,8 @@ MainMenuState::MainMenuState(
 
     _playButton = std::make_shared<ui::Button>(resourceManager);
     _playButton->setText("Play Game");
-    _playButton->setPosition(math::Vector2f(0.35f, 0.35f));
-    _playButton->setSize(math::Vector2f(0.3f, 0.1f));
+    _playButton->setPosition(math::Vector2f(672.f, 378.f));
+    _playButton->setSize(math::Vector2f(576.f, 108.f));
     _playButton->setNormalColor({100, 150, 100});
     _playButton->setHoveredColor({150, 200, 150});
     _playButton->setFocusedColor({255, 215, 0});
@@ -51,23 +51,23 @@ MainMenuState::MainMenuState(
         }
     });
     _playButton->setOnActivated([this]() {
-        auto network = this->_resourceManager->get<ClientNetwork>();
-        if (network && network->isConnected()) {
+        // auto network = this->_resourceManager->get<ClientNetwork>();
+        // if (network && network->isConnected()) {
             this->_gsm->requestStateChange(std::make_shared<DevState>(this->_gsm,
                 this->_resourceManager));
-        } else {
-            debug::Debug::printDebug(network ? network->isDebugMode() : false,
-                "[MainMenu] Cannot start game: Not connected to server.",
-                debug::debugType::NETWORK,
-                debug::debugLevel::WARNING);
-        }
+        // } else {
+        //     debug::Debug::printDebug(network ? network->isDebugMode() : false,
+        //         "[MainMenu] Cannot start game: Not connected to server.",
+        //         debug::debugType::NETWORK,
+        //         debug::debugLevel::WARNING);
+        // }
     });
     _uiManager->addElement(_playButton);
 
     _quitButton = std::make_shared<ui::Button>(resourceManager);
     _quitButton->setText("Quit");
-    _quitButton->setPosition(math::Vector2f(0.35f, 0.5f));
-    _quitButton->setSize(math::Vector2f(0.3f, 0.1f));
+    _quitButton->setPosition(math::Vector2f(672.f, 540.f));
+    _quitButton->setSize(math::Vector2f(576.f, 108.f));
     _quitButton->setNormalColor({150, 100, 100});
     _quitButton->setHoveredColor({200, 150, 150});
     _quitButton->setFocusedColor({255, 69, 0});
@@ -81,8 +81,8 @@ MainMenuState::MainMenuState(
 
     _highContrastButton = std::make_shared<ui::Button>(resourceManager);
     _highContrastButton->setText("HC");
-    _highContrastButton->setPosition(math::Vector2f(0.92f, 0.02f));
-    _highContrastButton->setSize(math::Vector2f(0.06f, 0.06f));
+    _highContrastButton->setPosition(math::Vector2f(1766.f, 22.f));
+    _highContrastButton->setSize(math::Vector2f(115.f, 65.f));
     _highContrastButton->setNormalColor({100, 100, 150});
     _highContrastButton->setHoveredColor({150, 150, 200});
     _highContrastButton->setFocusedColor({100, 200, 255});
