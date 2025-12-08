a16e08362ee7c91b415c30d0abba6da1ade49308
Marin <marin.lamy@epitech.eu>
2025-12-08 10:11:14 +0100

    feat(tags): add AIMoverTag and AIShooterTag components

---
+++ common/components/tags/AIShooterTag.hpp
@@ -0,0 +1,21 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** AIShooterTag
+*/
+
+#ifndef AISHOOTERTAG_HPP
+#define AISHOOTERTAG_HPP
+
+#include "../base/AComponent.hpp"
+
+namespace ecs {
+
+struct AIShooterTag : public AComponent {
+    AIShooterTag() = default;
+};
+
+} // namespace ecs
+
+#endif /* !AISHOOTERTAG_HPP */
