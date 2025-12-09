6e8875cafb1614b8616a8c23a328d320b68c410f
Marin <marin.lamy@epitech.eu>
2025-12-09 23:08:32 +0100

    refactor(init): update resource manager to handle ServerInputProvider and streamline entity prefab management

--- server/gsm/states/scenes/InGame/InGameState.cpp
+++ server/gsm/states/scenes/InGame/InGameState.cpp
@@ -7,6 +7,7 @@
 
 #include "InGameState.hpp"
 #include <memory>
+#include <iostream>
 #include "../../../../../common/systems/systemManager/ISystemManager.hpp"
 #include "../../../../../common/systems/movement/MovementSystem.hpp"
 #include "../../../../../common/systems/movement/InputToVelocitySystem.hpp"
@@ -15,10 +16,17 @@
 #include "../../../../../common/systems/death/DeathSystem.hpp"
 #include "../../../../../common/systems/lifetime/LifetimeSystem.hpp"
 #include "../../../../../common/systems/score/ScoreSystem.hpp"
+#include "../../../../systems/input/ServerMovementInputSystem.hpp"
 #include "../../../../../common/Parser/Parser.hpp"
 #include "../../../../../common/Prefab/entityPrefabManager/EntityPrefabManager.hpp"
 #include "../../../../../common/constants.hpp"
-
+#include "../../../../../common/ECS/entity/registry/Registry.hpp"
+#include "../../../../../common/Prefab/entityPrefabManager/EntityPrefabManager.hpp"
+#include "../../../../../common/Parser/Parser.hpp"
+#include "../../../../../common/systems/ai/AIMovementSystem.hpp"
+#include "../../../../../common/systems/ai/AIShootingSystem.hpp"
+#include "../../../../../common/systems/interactions/InteractionSystem.hpp"
+#include "../../../../../common/systems/interactions/TriggerSystem.hpp"
 namespace gsm {
 
 InGameState::InGameState(std::shared_ptr<IGameStateMachine> gsm,
@@ -30,17 +38,29 @@ void InGameState::enter() {
     auto parser = _resourceManager->get<Parser>();
     if (parser) {
         parser->parseAllEntities(constants::CONFIG_PATH);
-        parser->parseMapFromFile("configs/map/map1.json");
     }
 
+    addSystem(std::make_shared<ecs::AIMovementSystem>());
+    addSystem(std::make_shared<ecs::AIShootingSystem>());
     addSystem(std::make_shared<ecs::InputToVelocitySystem>());
+    addSystem(std::make_shared<ecs::ServerMovementInputSystem>());
     addSystem(std::make_shared<ecs::MovementSystem>());
     addSystem(std::make_shared<ecs::ShootingSystem>());
     addSystem(std::make_shared<ecs::LifetimeSystem>());
     addSystem(std::make_shared<ecs::HealthSystem>());
     addSystem(std::make_shared<ecs::DeathSystem>());
     addSystem(std::make_shared<ecs::ScoreSystem>());
-    // ...
+    addSystem(std::make_shared<ecs::TriggerSystem>());
+    addSystem(std::make_shared<ecs::InteractionSystem>());
+
+
+    auto registry = _resourceManager->get<ecs::Registry>();
+    if (_resourceManager->has<EntityPrefabManager>()) {
+        auto prefabManager = _resourceManager->get<EntityPrefabManager>();
+        prefabManager->createEntityFromPrefab("player", _resourceManager->get<ecs::Registry>());
+    } else {
+        throw std::runtime_error("EntityPrefabManager not found in ResourceManager");
+    }
 }
 
 }  // namespace gsm
