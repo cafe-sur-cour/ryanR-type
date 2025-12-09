1abd7874d4f708f269abfd2801e318517405d2d0
eliotttesnier <eliott.tesnier@epitech.eu>
2025-12-09 21:25:32 +0100

    feat(prefab): update instantiate method to include factory and context parameters

--- common/Prefab/APrefab.cpp
+++ common/Prefab/APrefab.cpp
@@ -8,6 +8,14 @@
 #include "APrefab.hpp"
 #include <memory>
 
-ecs::Entity APrefab::instantiate(const std::shared_ptr<ecs::Registry> &registry) {
+ecs::Entity APrefab::instantiate(
+    const std::shared_ptr<ecs::Registry>& registry,
+    const std::shared_ptr<ecs::IEntityFactory>& factory,
+    const ecs::EntityCreationContext& context
+) {
+    return factory->createEntity(registry, context);
+}
+
+ecs::Entity APrefab::instantiate(const std::shared_ptr<ecs::Registry>& registry) {
     return registry->createEntity();
 }
