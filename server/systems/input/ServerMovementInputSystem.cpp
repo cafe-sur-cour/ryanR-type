08bdc8df36cc07ef5cceb123901d7d68fe3166eb
Marin <marin.lamy@epitech.eu>
2025-12-09 23:08:06 +0100

    feat(input): implement ServerMovementInputSystem for handling player movement input

---
+++ server/systems/input/ServerMovementInputSystem.cpp
@@ -0,0 +1,100 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** ServerMovementInputSystem
+*/
+
+#include <cmath>
+#include <iostream>
+#include <memory>
+
+#include "ServerMovementInputSystem.hpp"
+#include "../../../common/types/Vector2f.hpp"
+#include "../../../common/constants.hpp"
+#include "../../../common/components/tags/ControllableTag.hpp"
+#include "../../../common/components/temporary/InputIntentComponent.hpp"
+#include "../../../common/components/tags/PlayerTag.hpp"
+#include "../../../common/systems/input/InputNormalizer.hpp"
+
+namespace ecs {
+
+ServerMovementInputSystem::ServerMovementInputSystem() {
+}
+
+math::Vector2f ServerMovementInputSystem::normalizeDirection(const math::Vector2f &direction) const {
+    return InputNormalizer::normalizeDirection(direction);
+}
+
+void ServerMovementInputSystem::update(
+    std::shared_ptr<ResourceManager> resourceManager,
+    std::shared_ptr<Registry> registry,
+    float deltaTime) {
+    (void)deltaTime;
+
+    if (!resourceManager->has<ecs::ServerInputProvider>()) {
+        return;
+    }
+
+    auto serverInputProvider = resourceManager->get<ecs::ServerInputProvider>();
+
+    // plus tard metre un composant qui lie au clientID pour mieux identifier
+    auto view = registry->view<ControllableTag, PlayerTag>();
+
+    std::vector<size_t> connectedClients = serverInputProvider->getConnectedClients();
+    if (connectedClients.empty())
+        return;
+
+    for (size_t clientID : connectedClients) {
+        for (auto entityId : view) {
+            float axisX = serverInputProvider->getActionAxis(InputAction::MOVE_X, clientID);
+            float axisY = serverInputProvider->getActionAxis(InputAction::MOVE_Y, clientID);
+            math::Vector2f movementDirection(axisX, axisY);
+            movementDirection = normalizeDirection(movementDirection);
+            updateInputIntent(registry, entityId, movementDirection);
+        }
+    }
+}
+
+math::Vector2f ServerMovementInputSystem::getMovementDirection(
+    std::shared_ptr<IInputProvider> inputProvider, size_t clientID
+) const {
+    math::Vector2f direction(0.0f, 0.0f);
+    direction.setX(inputProvider->getActionAxis(InputAction::MOVE_X, clientID));
+    direction.setY(inputProvider->getActionAxis(InputAction::MOVE_Y, clientID));
+
+    direction = normalizeDirection(direction);
+    math::Vector2f analogInput = getAnalogStickInput(inputProvider, clientID);
+    if (std::fabs(analogInput.getX()) > constants::EPS ||
+        std::fabs(analogInput.getY()) > constants::EPS) {
+        direction = analogInput;
+    }
+
+    return direction;
+}
+
+math::Vector2f ServerMovementInputSystem::getAnalogStickInput(
+    std::shared_ptr<IInputProvider> inputProvider, size_t clientID) const {
+    (void)inputProvider;
+    (void)clientID;
+    return math::Vector2f(0.0f, 0.0f);
+}
+
+void ServerMovementInputSystem::updateInputIntent(
+    std::shared_ptr<Registry> registry,
+    Entity entityId,
+    const math::Vector2f &direction) {
+
+    registry->registerComponent<InputIntentComponent>();
+    auto inputIntent = std::make_shared<InputIntentComponent>(direction);
+
+    if (registry->hasComponent<InputIntentComponent>(entityId)) {
+        auto existingIntent = registry->getComponent<InputIntentComponent>(entityId);
+
+        existingIntent->setDirection(direction);
+    } else {
+        registry->addComponent(entityId, inputIntent);
+    }
+}
+
+}  // namespace ecs
