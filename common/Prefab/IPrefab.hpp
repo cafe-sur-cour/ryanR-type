43aa24a5233f92ec5ba44c920afc2f10eccfbab1
eliotttesnier <eliott.tesnier@epitech.eu>
2025-12-09 21:25:32 +0100

    feat(prefab): update instantiate method to include factory and context parameters

--- common/Prefab/IPrefab.hpp
+++ common/Prefab/IPrefab.hpp
@@ -10,11 +10,20 @@
 #include <memory>
 #include "../ECS/entity/registry/Registry.hpp"
 #include "../ECS/entity/Entity.hpp"
+#include "../ECS/entity/EntityCreationContext.hpp"
+#include "../ECS/entity/factory/IEntityFactory.hpp"
 
 class IPrefab {
     public:
         virtual ~IPrefab() = default;
-        virtual ecs::Entity instantiate(const std::shared_ptr<ecs::Registry> &registry) = 0;
+
+        virtual ecs::Entity instantiate(
+            const std::shared_ptr<ecs::Registry>& registry,
+            const std::shared_ptr<ecs::IEntityFactory>& factory,
+            const ecs::EntityCreationContext& context = ecs::EntityCreationContext::forLocalClient()
+        ) = 0;
+
+        virtual ecs::Entity instantiate(const std::shared_ptr<ecs::Registry>& registry) = 0;
 };
 
 #endif /* !IPREFAB_HPP_ */
