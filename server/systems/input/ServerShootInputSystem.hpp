48aa8f5f2b68c06f8ab6d4e19b7c71e7bf29d9f5
Marin <marin.lamy@epitech.eu>
2025-12-10 11:32:22 +0100

    feat(input): implement ServerShootInputSystem for handling shoot intents

---
+++ server/systems/input/ServerShootInputSystem.hpp
@@ -0,0 +1,33 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** ServerShootInputSystem
+*/
+
+#ifndef SERVERSHOOTINPUTSYSTEM_HPP_
+#define SERVERSHOOTINPUTSYSTEM_HPP_
+
+#include "../../../common/systems/base/ASystem.hpp"
+#include "../../../common/components/temporary/ShootIntentComponent.hpp"
+#include "../../../common/InputMapping/IInputProvider.hpp"
+#include "../../../common/InputMapping/InputAction.hpp"
+#include "../../initResourcesManager/ServerInputProvider.hpp"
+#include <memory>
+
+namespace ecs {
+
+class ServerShootInputSystem : public ASystem {
+    public:
+        ServerShootInputSystem();
+        ~ServerShootInputSystem() = default;
+
+        void update(std::shared_ptr<ResourceManager> resourceManager, std::shared_ptr<Registry> registry, float deltaTime) override;
+
+    private:
+        void updateShootIntent(std::shared_ptr<Registry> registry, Entity entityId);
+};
+
+} // namespace ecs
+
+#endif /* !SERVERSHOOTINPUTSYSTEM_HPP_ */
