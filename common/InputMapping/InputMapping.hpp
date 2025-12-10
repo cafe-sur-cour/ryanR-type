7ba32486aff5fed36b42078ee6009fd5a7b14f04
eliotttesnier <eliott.tesnier@epitech.eu>
2025-12-10 16:50:38 +0100

    feat(InputMapping): add remappable actions and update key bindings

--- common/InputMapping/InputMapping.hpp
+++ common/InputMapping/InputMapping.hpp
@@ -15,20 +15,44 @@
 
 namespace ecs {
 
+struct RemappableKeyBinding {
+    gfx::EventType primary;
+    gfx::EventType secondary;
+
+    RemappableKeyBinding()
+        : primary(gfx::EventType::NOTHING), secondary(gfx::EventType::NOTHING) {}
+    RemappableKeyBinding(gfx::EventType p, gfx::EventType s)
+        : primary(p), secondary(s) {}
+};
+
 struct InputMapping {
-    std::map<InputAction, std::pair<gfx::EventType, gfx::EventType>> remappableKeys;
+    std::map<RemappableAction, RemappableKeyBinding> remappableKeys;
     std::map<InputAction, std::map<gfx::EventType, float>> fixedMappings;
 
     std::map<InputAction, std::map<gfx::EventType, float>> getAllMappings() const {
         std::map<InputAction, std::map<gfx::EventType, float>> all = fixedMappings;
-        for (const auto& [action, pair] : remappableKeys) {
-            if (pair.first != gfx::EventType::NOTHING) {
-                all[action][pair.first] = 1.0f;
+
+        for (const auto& [action, binding] : remappableKeys) {
+            InputAction inputAction;
+            switch (action) {
+                case RemappableAction::MOVE_LEFT: inputAction = InputAction::MOVE_X; break;
+                case RemappableAction::MOVE_RIGHT: inputAction = InputAction::MOVE_X; break;
+                case RemappableAction::MOVE_UP: inputAction = InputAction::MOVE_Y; break;
+                case RemappableAction::MOVE_DOWN: inputAction = InputAction::MOVE_Y; break;
+                case RemappableAction::SHOOT: inputAction = InputAction::SHOOT; break;
             }
-            if (pair.second != gfx::EventType::NOTHING) {
-                all[action][pair.second] = 1.0f;
+
+            float value = (action == RemappableAction::MOVE_LEFT ||
+                action == RemappableAction::MOVE_UP) ? -1.0f : 1.0f;
+
+            if (binding.primary != gfx::EventType::NOTHING) {
+                all[inputAction][binding.primary] = value;
+            }
+            if (binding.secondary != gfx::EventType::NOTHING) {
+                all[inputAction][binding.secondary] = value;
             }
         }
+
         return all;
     }
 };
--- common/InputMapping/InputMappingManager.hpp
+++ common/InputMapping/InputMappingManager.hpp
@@ -25,16 +25,10 @@ public:
     const InputMapping& getMapping() const;
     InputMapping& getMutableMapping();
 
-    gfx::EventType getKeyboardKeyForAction(InputAction action) const;
-
-    gfx::EventType getKeyboardKeyForActionDirection(InputAction action, float direction) const;
-
-    void remapKeyboardKey(InputAction action, gfx::EventType oldKey, gfx::EventType newKey);
-
-    std::vector<gfx::EventType> getKeyboardKeysForAction(InputAction action) const;
+    gfx::EventType getKeyForRemappableAction(RemappableAction action, bool getPrimary = true) const;
+    void remapKey(RemappableAction action, gfx::EventType newKey, bool setPrimary);
 
     static std::string eventTypeToString(gfx::EventType eventType);
-
     static bool isKeyboardKey(gfx::EventType eventType);
 
 private:
