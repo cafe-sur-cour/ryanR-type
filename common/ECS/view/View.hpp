1ed09f4cd8d225bdaf79c59310d000740226be30
albanrss <alban.roussee@epitech.eu>
2025-12-15 13:28:06 +0100

    refactor(Group): remove unused Group class and related tests

--- common/ECS/view/View.hpp
+++ common/ECS/view/View.hpp
@@ -42,34 +42,6 @@ class View {
         std::shared_ptr<class Registry> _registry;
 };
 
-template <typename... Components>
-class Group {
-    public:
-        Group(std::shared_ptr<class Registry> registry);
-
-        class Iterator;
-
-        Iterator begin();
-        Iterator end();
-
-        class Iterator {
-            public:
-                Iterator(std::shared_ptr<class Registry> registry, size_t entityId, size_t maxEntityId);
-                bool operator!=(const Iterator& other) const;
-                Iterator& operator++();
-                size_t operator*() const;
-
-            private:
-                bool hasAllComponents() const;
-                std::shared_ptr<class Registry> _registry;
-                size_t _entityId;
-                size_t _maxEntityId;
-        };
-
-    private:
-        std::shared_ptr<class Registry> _registry;
-};
-
 }  // namespace ecs
 
 #include "View.tpp"
