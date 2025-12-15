0a296772606380d0b09ad3f99f22f0e7931cbd5a
Albane Merian <albane.merian@epitech.eu>
2025-12-15 16:36:52 +0100

    fix(packFloat): Update the pack Float function so that it initialize the value and doesn't cause memory leaks

--- server/main.cpp
+++ server/main.cpp
@@ -26,7 +26,7 @@ int main(int ac, char **av) {
     std::cout << std::endl << "------------------------" << std::endl;
     try {
         core.init();
-        // core.loop();
+        core.loop();
     } catch (const err::IError &e) {
         std::cerr << e.getDetails() << std::endl;
         return (84);
