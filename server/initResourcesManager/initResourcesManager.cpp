266ac9880f6f1e331d7d54809591430021fac624
Marin <marin.lamy@epitech.eu>
2025-12-16 11:57:17 +0100

    feat(PacketManager): add END_GAME_PACKET type handling in pack method

--- server/initResourcesManager/initResourcesManager.cpp
+++ server/initResourcesManager/initResourcesManager.cpp
@@ -19,6 +19,7 @@
 #include "../../common/components/permanent/NetworkIdComponent.hpp"
 #include "../../common/debug.hpp"
 #include "initResourcesManager.hpp"
+#include "../gsm/gsmStates.hpp"
 
 std::shared_ptr<ResourceManager> initResourcesManager(std::shared_ptr<rserv::Server> server,
     std::shared_ptr<ecs::Registry> registry, std::shared_ptr<Parser> parser,
@@ -103,5 +104,6 @@ std::shared_ptr<ResourceManager> initResourcesManager(std::shared_ptr<rserv::Ser
     resourceManager->add<Parser>(parser);
     resourceManager->add<ecs::ISystemManager>(systemsManager);
     resourceManager->add<gsm::GameStateMachine>(gameStateMachine);
+    resourceManager->add<gsm::GameStateType>(std::make_shared<gsm::GameStateType>(gsm::BOOT));
     return resourceManager;
 }
