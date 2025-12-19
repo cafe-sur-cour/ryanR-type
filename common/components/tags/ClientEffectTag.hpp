3e82c4daaa4b9b4ed30521356633ee3b89ed1037
eliotttesnier <eliott.tesnier@epitech.eu>
2025-12-19 21:08:34 +0100

    feat(tags): add ClientEffectTag and register it in TagRegistry

---
+++ common/components/tags/ClientEffectTag.hpp
@@ -0,0 +1,23 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** ClientEffectTag
+*/
+
+#ifndef CLIENTEFFECTTAG_HPP_
+#define CLIENTEFFECTTAG_HPP_
+
+#include "../base/AComponent.hpp"
+
+namespace ecs {
+
+class ClientEffectTag : public AComponent {
+    public:
+        ClientEffectTag() = default;
+        ~ClientEffectTag() = default;
+};
+
+}
+
+#endif /* !CLIENTEFFECTTAG_HPP_ */
