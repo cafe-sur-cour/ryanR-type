695a7e2fd1994fdd425670913a22ad177421bec5
Matisse-M <matisse.marsac@epitech.eu>
2026-01-02 17:03:28 +0100

    docs(network): add documentation about changes

--- libs/Network/common.hpp
+++ libs/Network/common.hpp
@@ -8,18 +8,13 @@
 #ifndef COMMON_HPP_
     #define COMMON_HPP_
 
-#include "Asio/AsioErrorCode.hpp"
-#include "Asio/AsioAddress.hpp"
-#include "Asio/AsioEndpoint.hpp"
-#include "Asio/AsioSocket.hpp"
-#include "Asio/AsioResolver.hpp"
-#include "Asio/AsioEventLoop.hpp"
+#include "NetworkConfig.hpp"
 
-using NetworkErrorCode = net::AsioErrorCode;
-using NetworkAddress = net::AsioAddress;
-using NetworkEndpoint = net::AsioEndpoint;
-using NetworkSocket = net::AsioSocket;
-using NetworkResolver = net::AsioResolver;
-using NetworkEventLoop = net::AsioEventLoop;
+using NetworkErrorCode = net::NetworkErrorCode;
+using NetworkAddress = net::NetworkAddress;
+using NetworkEndpoint = net::NetworkEndpoint;
+using NetworkSocket = net::NetworkSocket;
+using NetworkResolver = net::NetworkResolver;
+using NetworkEventLoop = net::NetworkEventLoop;
 
 #endif /* !COMMON_HPP_ */
