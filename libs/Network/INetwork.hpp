fe264d83cd8b87e969d9069fd888bf4cdd4a5eae
Matisse-M <matisse.marsac@epitech.eu>
2026-01-02 14:27:29 +0100

    refactor(network): update network interface using net lib wrapped

--- libs/Network/INetwork.hpp
+++ libs/Network/INetwork.hpp
@@ -10,11 +10,14 @@
 
 #include <vector>
 #include <functional>
-#include "NetworkEndpoint.hpp"
+#include <memory>
+#include "INetworkEndpoint.hpp"
 #include "../Packet/IPacketManager.hpp"
 #include "../Buffer/IBuffer.hpp"
 
 namespace net {
+
+class AsioEndpoint; // Forward declaration
 
 enum class ConnectionState {
     DISCONNECTED,
@@ -33,11 +36,11 @@ class INetwork {
         virtual void init(uint16_t port, const std::string host) = 0;
         virtual void stop() = 0;
 
-        virtual bool sendTo(const NetworkEndpoint& endpoint, std::vector<uint8_t> packet) = 0;
-        virtual bool broadcast(const std::vector<NetworkEndpoint>& endpoints, std::vector<uint8_t> data) = 0;
+        virtual bool sendTo(const INetworkEndpoint& endpoint, std::vector<uint8_t> packet) = 0;
+        virtual bool broadcast(const std::vector<std::shared_ptr<INetworkEndpoint>>& endpoints, const std::vector<uint8_t>& data) = 0;
         virtual bool hasIncomingData() const = 0;
         virtual std::vector<uint8_t> receiveFrom(const uint8_t &connectionId) = 0;
-        virtual std::pair<NetworkEndpoint, std::vector<uint8_t>> receiveAny() = 0;
+        virtual std::pair<std::shared_ptr<INetworkEndpoint>, std::vector<uint8_t>> receiveAny() = 0;
 
         virtual void setConnectionCallback(std::function<void(int)> onConnect) = 0;
         virtual void setDisconnectionCallback(std::function<void(int)> onDisconnect) = 0;
