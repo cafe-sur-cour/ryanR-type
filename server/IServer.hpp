f948f9b134d9387567162a1cc541607100ebd64f
Matisse-M <matisse.marsac@epitech.eu>
2026-01-03 14:37:20 +0100

    fix(network): remove useless french comment

--- server/IServer.hpp
+++ server/IServer.hpp
@@ -19,44 +19,46 @@
 #include "../common/constants.hpp"
 
 namespace rserv {
+
 class IServer {
-public:
-    virtual ~IServer() = default;
+    public:
+        virtual ~IServer() = default;
 
-    virtual void init() = 0;
-    virtual void start() = 0;
-    virtual void stop() = 0;
+        virtual void init() = 0;
+        virtual void start() = 0;
+        virtual void stop() = 0;
 
-    virtual void setConfig(std::shared_ptr<ServerConfig> config) = 0;
-    virtual std::shared_ptr<ServerConfig> getConfig() const = 0;
-    virtual uint16_t getPort() const = 0;
-    virtual void setPort(uint16_t port) = 0;
+        virtual void setConfig(std::shared_ptr<ServerConfig> config) = 0;
+        virtual std::shared_ptr<ServerConfig> getConfig() const = 0;
+        virtual uint16_t getPort() const = 0;
+        virtual void setPort(uint16_t port) = 0;
 
-    virtual int getState() const = 0;
-    virtual void setState(int state) = 0;
+        virtual int getState() const = 0;
+        virtual void setState(int state) = 0;
 
-    virtual operator int() const noexcept = 0;
+        virtual operator int() const noexcept = 0;
 
-    virtual std::shared_ptr<net::INetwork> getNetwork() const = 0;
-    virtual void setNetwork(std::shared_ptr<net::INetwork> network) = 0;
+        virtual std::shared_ptr<net::INetwork> getNetwork() const = 0;
+        virtual void setNetwork(std::shared_ptr<net::INetwork> network) = 0;
 
-    virtual void onClientConnected(uint8_t idClient) = 0;
-    virtual void onClientDisconnected(uint8_t idClient) = 0;
-    virtual void onPacketReceived(uint8_t idClient, const pm::IPacketManager &packet) = 0;
+        virtual void onClientConnected(uint8_t idClient) = 0;
+        virtual void onClientDisconnected(uint8_t idClient) = 0;
+        virtual void onPacketReceived(uint8_t idClient, const pm::IPacketManager &packet) = 0;
 
-    virtual void processIncomingPackets() = 0;
-    virtual bool processConnections(std::pair<std::shared_ptr<net::INetworkEndpoint>, std::vector<uint8_t>> client) = 0;
-    virtual bool processDisconnections(uint8_t idClient) = 0;
-    virtual bool processEvents(uint8_t idClient) = 0;
-    virtual bool processEndOfGame(uint8_t idClient) = 0;
+        virtual void processIncomingPackets() = 0;
+        virtual bool processConnections(std::pair<std::shared_ptr<net::INetworkEndpoint>, std::vector<uint8_t>> client) = 0;
+        virtual bool processDisconnections(uint8_t idClient) = 0;
+        virtual bool processEvents(uint8_t idClient) = 0;
+        virtual bool processEndOfGame(uint8_t idClient) = 0;
 
-    virtual std::vector<uint8_t> getConnectedClients() const = 0;
-    virtual std::vector<std::shared_ptr<net::INetworkEndpoint>> getConnectedClientEndpoints() const = 0;
-    virtual size_t getClientCount() const = 0;
+        virtual std::vector<uint8_t> getConnectedClients() const = 0;
+        virtual std::vector<std::shared_ptr<net::INetworkEndpoint>> getConnectedClientEndpoints() const = 0;
+        virtual size_t getClientCount() const = 0;
 
-    virtual std::shared_ptr<std::queue<std::tuple<uint8_t, constants::EventType, double>>> getEventQueue() = 0;
-    virtual bool hasEvents() const = 0;
+        virtual std::shared_ptr<std::queue<std::tuple<uint8_t, constants::EventType, double>>> getEventQueue() = 0;
+        virtual bool hasEvents() const = 0;
 };
-} // namespace rserv = r-type server
+
+}
 
 #endif /* !ISERVER_HPP_ */
