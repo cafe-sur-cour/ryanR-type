e6088c34d4576c26b6ca441ba156a6d1cf7b18c4
albanrss <alban.roussee@epitech.eu>
2026-01-05 17:38:27 +0100

    feat(ScriptingComponent): enhance Lua script handling with protected functions and add clearLuaReferences method

--- common/components/permanent/ScriptingComponent.cpp
+++ common/components/permanent/ScriptingComponent.cpp
@@ -23,8 +23,8 @@ void ecs::ScriptingComponent::init(sol::state& lua, size_t entityId) {
         sol::table metatable = lua.create_table();
         metatable["__index"] = lua["_G"];
         _env[sol::metatable_key] = metatable;
-        sol::function chunk = script;
-        sol::unsafe_function_result result = chunk(_env);
+        sol::protected_function chunk(script);
+        sol::protected_function_result result = chunk(_env);
         if (!result.valid()) {
             sol::error err = result;
             throw err::ScriptingError("Failed to run script: " +
@@ -35,13 +35,13 @@ void ecs::ScriptingComponent::init(sol::state& lua, size_t entityId) {
     std::vector<std::string> defaults = {constants::INIT_FUNCTION, constants::UPDATE_FUNCTION};
     defaults.insert(defaults.end(), _additionalFunctions.begin(), _additionalFunctions.end());
     for (const auto& def : defaults) {
-        sol::function fn = _env[def];
-        if (fn.valid())
-            _functions[def] = fn;
+        sol::optional<sol::protected_function> fn = _env[def];
+        if (fn)
+            _functions[def] = *fn;
     }
-    sol::function initFunc = _env[constants::INIT_FUNCTION];
-    if (initFunc.valid()) {
-        sol::unsafe_function_result result = initFunc(entityId);
+    sol::optional<sol::protected_function> initFunc = _env[constants::INIT_FUNCTION];
+    if (initFunc) {
+        sol::protected_function_result result = (*initFunc)(entityId);
         if (!result.valid()) {
             sol::error err = result;
             throw err::ScriptingError("Failed to run init function: " +
