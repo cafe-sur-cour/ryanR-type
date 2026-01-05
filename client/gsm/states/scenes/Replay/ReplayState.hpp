8879fd3efdaa409173290bca9df45aac67765ba7
eliotttesnier <eliott.tesnier@epitech.eu>
2026-01-05 14:05:18 +0100

    feat(replay): add ReplayState implementation and CMake configuration

---
+++ client/gsm/states/scenes/Replay/ReplayState.hpp
@@ -0,0 +1,67 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** ReplayState
+*/
+
+#ifndef REPLAYSTATE_HPP_
+#define REPLAYSTATE_HPP_
+
+#include "../../base/AGameState.hpp"
+#include "../../../../../common/gsm/IGameStateMachine.hpp"
+#include "../../../../input/MouseInputHandler.hpp"
+#include "../../../../ui/elements/focusable/Button.hpp"
+#include "../../../../ui/elements/Text.hpp"
+#include "../../../../ui/elements/Background.hpp"
+#include "../../../../ui/manager/UIManager.hpp"
+#include "../../../../ui/core/UILayout.hpp"
+#include "../../../../../common/types/Vector2f.hpp"
+#include "../../../../../common/interfaces/IAudio.hpp"
+#include "../../../../components/temporary/MusicIntentComponent.hpp"
+#include "../../../../SettingsConfig.hpp"
+#include <nlohmann/json.hpp>
+
+namespace gsm {
+
+class ReplayState : public AGameState {
+public:
+    ReplayState(std::shared_ptr<IGameStateMachine> gsm, std::shared_ptr<ResourceManager> resourceManager);
+    ~ReplayState() override = default;
+
+    void enter() override;
+    void update(float deltaTime) override;
+    void exit() override;
+
+private:
+    void renderUI();
+    void renderReplaySprites();
+    void renderParallaxBackground(const nlohmann::json& parallaxData, std::shared_ptr<gfx::IWindow> window);
+    void renderHealthBar(const nlohmann::json& healthBarData, std::shared_ptr<gfx::IWindow> window);
+    void renderText(const nlohmann::json& textData, std::shared_ptr<gfx::IWindow> window);
+    void renderRectangle(const nlohmann::json& rectangleData, std::shared_ptr<gfx::IWindow> window);
+    void renderHitbox(const nlohmann::json& hitboxData, std::shared_ptr<gfx::IWindow> window);
+    void updateViewForFrame(const nlohmann::json& frame);
+    void loadReplay();
+    void playReplay(float deltaTime);
+    void processAudioForFrame(const nlohmann::json& frame);
+
+    std::unique_ptr<MouseInputHandler> _mouseHandler;
+    std::unique_ptr<ui::UIManager> _uiManager;
+
+    std::shared_ptr<ui::Background> _background;
+    std::shared_ptr<ui::Button> _backButton;
+    std::shared_ptr<ui::Button> _playButton;
+    std::shared_ptr<ui::Text> _statusText;
+
+    // Replay data
+    std::vector<nlohmann::json> _frames;
+    float _replayTime;
+    float _totalReplayTime;
+    size_t _currentFrameIndex;
+    bool _isPlaying;
+};
+
+}  // namespace gsm
+
+#endif /* !REPLAYSTATE_HPP_ */
