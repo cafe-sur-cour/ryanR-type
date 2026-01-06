26fa74f47598a06f14a210ba30f8740f8c4814ba
albanrss <alban.roussee@epitech.eu>
2026-01-06 10:55:34 +0100

    feat(parser): add BackGroundMusicTag component registration

---
+++ client/components/tags/BackGroundMusicTag.cpp
@@ -0,0 +1,12 @@
+/*
+** EPITECH PROJECT, 2025
+** ryanR-type
+** File description:
+** BackGroundMusicTag registration
+*/
+
+#include "BackGroundMusicTag.hpp"
+#include "../../../common/Parser/ComponentRegistry/ComponentRegistrar.hpp"
+#include "../../../common/constants.hpp"
+
+REGISTER_TAG_COMPONENT(ecs::BackGroundMusicTag, constants::BACKGROUNDMUSICTAG)
