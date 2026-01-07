2fb1de58229937c453e26a6a4a2db11bdc10a1c9
eliotttesnier <eliott.tesnier@epitech.eu>
2026-01-07 15:44:40 +0100

    fix(RoundedRectangleShape): ensure M_PI is defined for compatibility

--- libs/Multimedia/SFML/RoundedRectangleShape.cpp
+++ libs/Multimedia/SFML/RoundedRectangleShape.cpp
@@ -9,6 +9,10 @@
 #include "RoundedRectangleShape.hpp"
 #include <cmath>
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 namespace sf {
 
 RoundedRectangleShape::RoundedRectangleShape() :
