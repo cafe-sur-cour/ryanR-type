48bbf03ec65dfb7f8b9a3707e4d11b0086bd73c3
eliotttesnier <eliott.tesnier@epitech.eu>
2026-01-11 05:31:16 +0100

    feat(results): add leave button and mouse input handling in ResultsState

--- client/gsm/states/scenes/Results/ResultsState.hpp
+++ client/gsm/states/scenes/Results/ResultsState.hpp
@@ -13,6 +13,9 @@
 #include "../../../../../common/interfaces/IWindow.hpp"
 #include "ui/manager/UIManager.hpp"
 #include "ui/elements/Text.hpp"
+#include "ui/elements/focusable/Button.hpp"
+#include "ui/core/UILayout.hpp"
+#include "../../../../input/MouseInputHandler.hpp"
 #include "../../../../colors.hpp"
 
 namespace gsm {
@@ -28,10 +31,14 @@ class ResultsState : public AGameState {
 
     private:
         void updateUserStats();
+        void renderUI();
 
         bool _isWin;
         std::unique_ptr<ui::UIManager> _uiManager;
+        std::unique_ptr<MouseInputHandler> _mouseHandler;
         std::shared_ptr<ui::Text> _resultText;
+        std::shared_ptr<ui::Button> _leaveButton;
+        std::shared_ptr<ui::UILayout> _bottomRightLayout;
 };
 
 }  // namespace gsm
