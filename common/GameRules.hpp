f1d23a1a81277ee3e72362bedf94c922f603aa33
eliotttesnier <eliott.tesnier@epitech.eu>
2026-01-11 22:37:09 +0100

    refactor(GameRules): update method signatures to use scoped enums for clarity

--- common/GameRules.hpp
+++ common/GameRules.hpp
@@ -24,18 +24,18 @@ class GameRules {
         GameRules();
         ~GameRules() = default;
 
-        void setGamemode(Gamemode gamemode);
-        Gamemode getGamemode() const;
+        void setGamemode(GameRules::Gamemode gamemode);
+        GameRules::Gamemode getGamemode() const;
 
-        void setDifficulty(Difficulty difficulty);
-        Difficulty getDifficulty() const;
+        void setDifficulty(GameRules::Difficulty difficulty);
+        GameRules::Difficulty getDifficulty() const;
 
         void setCrossfire(bool crossfire);
         bool getCrossfire() const;
 
     private:
-        Gamemode _gamemode;
-        Difficulty _difficulty;
+        GameRules::Gamemode _gamemode;
+        GameRules::Difficulty _difficulty;
         bool _crossfire;
 };
 
