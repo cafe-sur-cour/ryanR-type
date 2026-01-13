39972736dd134140a42ddcc694a60c80f7519fa1
eliotttesnier <eliott.tesnier@epitech.eu>
2026-01-14 00:11:31 +0100

    fix(powerups): correct charge logic to increase max charge on interaction

--- configs/scripts/powerUpChargeTime.lua
+++ configs/scripts/powerUpChargeTime.lua
@@ -7,8 +7,8 @@ end
 function OnInteract(entity, interactorEntity)
     if isEntityPlayer(interactorEntity) then
         local currentMaxCharge = getMaxCharge(interactorEntity)
-        if currentMaxCharge > 0.6 then
-            setMaxCharge(interactorEntity, currentMaxCharge - 0.2)
+        if currentMaxCharge < 5.0 then
+            setMaxCharge(interactorEntity, currentMaxCharge + 0.5)
             createDeathIntent(entity)
         end
     end
