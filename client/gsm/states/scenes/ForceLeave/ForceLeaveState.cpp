3fbd35dd498c9c4eba8237c749c36f0a3ae962f1
Albane Merian <albane.merian@epitech.eu>
2026-01-15 10:47:23 +0100

    rm(coment): Remove old commented loop

--- client/gsm/states/scenes/ForceLeave/ForceLeaveState.cpp
+++ client/gsm/states/scenes/ForceLeave/ForceLeaveState.cpp
@@ -151,26 +151,6 @@ void ForceLeaveState::update(float deltaTime) {
         _resourceManager->get<gfx::IWindow>()->closeWindow();
         return;
     }
-    // auto network = this->_resourceManager->get<ClientNetwork>();
-    // if (network && network->_shouldDisconnect) {
-    //     network->setLobbyCode("");
-    //     network->_isConnectedToLobby = false;
-    //     network->_isLobbyMaster = false;
-    //     network->_ready = false;
-    //     network->clearEntitiesAndMappings();
-    //     auto gsmPtr = _gsm.lock();
-    //     if (gsmPtr) {
-    //         auto mainMenuState =
-    //             std::make_shared<gsm::MainMenuState>(gsmPtr, _resourceManager);
-    //         gsmPtr->requestStateChange(mainMenuState);
-    //     }
-    //     auto IAudio = this->_resourceManager->get<gfx::IAudio>();
-    //     if (IAudio) {
-    //         IAudio->stopAllSounds();
-    //         IAudio->stopMusic();
-    //     }
-    //     network->_shouldDisconnect = false;
-    // }
     _uiManager->handleKeyboardInput(eventResult);
 
     math::Vector2f mousePos = _mouseHandler->getWorldMousePosition();
